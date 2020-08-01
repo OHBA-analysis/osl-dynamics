Fixed alpha scaling and covariance normalisation.

examples/one_subject_example1.py
@@ -1,11 +1,10 @@
 """Example script for running inference on real MEG data for one subject.
 
 - The data is stored on the BMRC cluster: /well/woolrich/shared/vrad
-- Data preparation is performed within V-RAD. This is in contrast to examples3.py,
-  which used data that's already been prepared.
-- Initialises the covariances with the identity matrix.
-- Achieves a dice coefficient of ~0.4 (when compared to the OSL HMM state time course).
-- Achieved a free energy of ~240,000.
+- Uses the final covariances inferred by an HMM fit from OSL.
+- Takes approximately 4 minutes to train (on compG017).
+- Achieves a dice coefficient of ~0.7 (when compared to the OSL HMM state time course).
+- Achieves a free energy of ~240,000.
 """
 
 print("Importing packages")
@@ -34,7 +33,7 @@ annealing_sharpness = 5
 
 n_epochs = 200
 n_epochs_annealing = 150
-n_epochs_burnin = 0
+n_epochs_burnin = 30
 
 dropout_rate_inference = 0.4
 dropout_rate_model = 0.4
@@ -49,21 +48,21 @@ learn_means = False
 learn_covariances = True
 
 alpha_xform = "softmax"
-learn_alpha_scaling = False
-normalize_covariances = False
+learn_alpha_scaling = True
+normalize_covariances = True
 
-n_initializations = 5
-n_epochs_initialization = 25
+n_initializations = None
+n_epochs_initialization = None
 
 # Read MEG data
 print("Reading MEG data")
-meg_data = data.Data("/well/woolrich/shared/vrad/preprocessed_data/subject1.mat")
-meg_data.prepare(n_embeddings=13, n_pca_components=80, whiten=True)
+meg_data = data.Data("/well/woolrich/shared/vrad/prepared_data/one_subject.mat")
 n_channels = meg_data.n_channels
 
-# Use defaults for the initial means and covariances
-means = None
-covariances = None
+# Priors: we use the covariance matrices inferred by fitting an HMM with OSL
+hmm = data.OSL_HMM("/well/woolrich/shared/vrad/hmm_fits/one_subject/hmm.mat")
+covariances = hmm.covariances
+means = np.zeros([n_states, n_channels], dtype=np.float32)
 
 # Build model
 model = create_model(
@@ -115,7 +114,6 @@ inf_stc = inf_stc.argmax(axis=1)
 inf_stc = array_ops.get_one_hot(inf_stc)
 
 # Find correspondance between state time courses
-hmm = data.OSL_HMM("/well/woolrich/shared/vrad/hmm_fits/one_subject/hmm.mat")
 matched_stc, matched_inf_stc = array_ops.match_states(hmm.viterbi_path, inf_stc)
 # plotting.compare_state_data(matched_stc, matched_inf_stc, filename="compare.png")
 

examples/simulation_example.py
@@ -1,29 +1,30 @@
-"""Example script for running inference on real MEG data for one subject.
+"""Example script for running inference on simulated HMM data.
 
-- The data is stored on the BMRC cluster: /well/woolrich/shared/vrad
-- Initialises the covariances with the identity matrix. This is in constrast to
-  example2.py, which uses the final covariances from an HMM fit using OSL.
-- Takes approximately 4 minutes to train (on compG017).
-- Achieves a dice coefficient of ~0.4 (when compared to the OSL HMM state time course).
-- Achieved a free energy of ~240,000.
+- This script sets a seed for the random number generators for reproducibility.
+- Should achieve a dice coefficient of ~0.94.
+- Takes approximately 1 minute to train (on compG017).
 """
 
 print("Importing packages")
 import numpy as np
 from vrad import array_ops, data
-from vrad.inference import metrics, tf_ops
+from vrad.inference import gmm, metrics, tf_ops
 from vrad.inference.models import create_model
+from vrad.simulation import HMMSimulation
 from vrad.utils import plotting
 
 # GPU settings
 tf_ops.gpu_growth()
 
-multi_gpu = True
+multi_gpu = False
 strategy = None
 
 # Settings
-n_states = 6
-sequence_length = 400
+n_samples = 25000
+observation_error = 0.2
+
+n_states = 5
+sequence_length = 100
 batch_size = 32
 
 learning_rate = 0.01
@@ -32,8 +33,8 @@ clip_normalization = None
 do_annealing = True
 annealing_sharpness = 5
 
-n_epochs = 200
-n_epochs_annealing = 150
+n_epochs = 100
+n_epochs_annealing = 80
 n_epochs_burnin = 0
 
 dropout_rate_inference = 0.4
@@ -52,17 +53,41 @@ alpha_xform = "softmax"
 learn_alpha_scaling = False
 normalize_covariances = False
 
-n_initializations = 5
-n_epochs_initialization = 25
+n_initializations = None
+n_epochs_initialization = None
 
-# Read MEG data
-print("Reading MEG data")
-meg_data = data.Data("/well/woolrich/shared/vrad/prepared_data/one_subject.mat")
+# Load state transition probability matrix and covariances of each state
+init_trans_prob = np.load("data/prob_000.npy")
+init_cov = np.load("data/state_000.npy")
+
+# Simulate data
+print("Simulating data")
+sim = HMMSimulation(
+    n_states=n_states,
+    trans_prob=init_trans_prob,
+    covariances=init_cov,
+    n_samples=n_samples,
+    observation_error=observation_error,
+    random_seed=123,
+)
+meg_data = data.Data(sim)
 n_channels = meg_data.n_channels
 
-# Use defaults for the initial means and covariances
-means = None
-covariances = None
+# Priors
+means, covariances = gmm.final_means_covariances(
+    meg_data,
+    n_states,
+    gmm_kwargs={
+        "n_init": 1,
+        "verbose": 2,
+        "verbose_interval": 50,
+        "max_iter": 10000,
+        "tol": 1e-6,
+    },
+    retry_attempts=1,
+    learn_means=False,
+    random_seed=124,
+)
 
 # Build model
 model = create_model(
@@ -104,8 +129,8 @@ prediction_dataset = meg_data.prediction_dataset(sequence_length, batch_size)
 print("Training model")
 history = model.fit(training_dataset, epochs=n_epochs, verbose=0, use_tqdm=True)
 
-# Inferred covariance matrices
-int_means, inf_cov = model.get_means_covariances()
+# Inferred covariances
+inf_means, inf_cov = model.get_means_covariances()
 # plotting.plot_matrices(inf_cov, filename="covariances.png")
 
 # Inferred state time courses
@@ -113,14 +138,9 @@ inf_stc = model.predict_states(prediction_dataset)
 inf_stc = inf_stc.argmax(axis=1)
 inf_stc = array_ops.get_one_hot(inf_stc)
 
-# Find correspondance between state time courses
-hmm = data.OSL_HMM("/well/woolrich/shared/vrad/hmm_fits/one_subject/hmm.mat")
-matched_stc, matched_inf_stc = array_ops.match_states(hmm.viterbi_path, inf_stc)
+# Find correspondance to ground truth state time courses
+matched_stc, matched_inf_stc = array_ops.match_states(sim.state_time_course, inf_stc)
 # plotting.compare_state_data(matched_stc, matched_inf_stc, filename="compare.png")
+# plotting.plot_state_time_courses(matched_stc, matched_inf_stc, filename='stc.png')
 
-# Dice coefficient
 print("Dice coefficient:", metrics.dice_coefficient(matched_stc, matched_inf_stc))
-
-# Free energy
-free_energy, ll_loss, kl_loss = model.free_energy(prediction_dataset, return_all=True)
-print(f"Free energy: {ll_loss} + {kl_loss} = {free_energy}")

examples/ten_subjects_example.py
@@ -1,14 +1,12 @@
-"""Example script for running inference on real MEG data for one subject.
+"""Example script for running inference on real MEG data for ten subjects.
 
 - The data is stored on the BMRC cluster: /well/woolrich/shared/vrad
-- Uses the final covariances inferred by an HMM fit from OSL.
-- Takes approximately 4 minutes to train (on compG017).
-- Achieves a dice coefficient of ~0.7 (when compared to the OSL HMM state time course).
-- Achieves a free energy of ~240,000.
+- Uses prepared data for 10 subjects.
+- Achieves a dice coefficient of ~0.4.
+- Achieves a free energy of ~490,000.
 """
 
 print("Importing packages")
-import numpy as np
 from vrad import array_ops, data
 from vrad.inference import metrics, tf_ops
 from vrad.inference.models import create_model
@@ -22,7 +20,7 @@ strategy = None
 
 # Settings
 n_states = 6
-sequence_length = 400
+sequence_length = 800
 batch_size = 32
 
 learning_rate = 0.01
@@ -31,9 +29,9 @@ clip_normalization = None
 do_annealing = True
 annealing_sharpness = 5
 
-n_epochs = 200
-n_epochs_annealing = 150
-n_epochs_burnin = 30
+n_epochs = 250
+n_epochs_annealing = 125
+n_epochs_burnin = 0
 
 dropout_rate_inference = 0.4
 dropout_rate_model = 0.4
@@ -41,28 +39,23 @@ dropout_rate_model = 0.4
 n_layers_inference = 1
 n_layers_model = 1
 
-n_units_inference = 64
-n_units_model = 64
+n_units_inference = 128
+n_units_model = 128
 
 learn_means = False
 learn_covariances = True
 
 alpha_xform = "softmax"
-learn_alpha_scaling = False
-normalize_covariances = False
+learn_alpha_scaling = True
+normalize_covariances = True
 
-n_initializations = None
-n_epochs_initialization = None
+n_initializations = 4
+n_epochs_initialization = 35
 
 # Read MEG data
 print("Reading MEG data")
-meg_data = data.Data("/well/woolrich/shared/vrad/prepared_data/one_subject.mat")
+meg_data = data.Data("/well/woolrich/shared/vrad/prepared_data/ten_subjects_1.mat")
 n_channels = meg_data.n_channels
-
-# Priors: we use the covariance matrices inferred by fitting an HMM with OSL
-hmm = data.OSL_HMM("/well/woolrich/shared/vrad/hmm_fits/one_subject/hmm.mat")
-covariances = hmm.covariances
-means = np.zeros([n_states, n_channels], dtype=np.float32)
 
 # Build model
 model = create_model(
@@ -71,8 +64,8 @@ model = create_model(
     sequence_length=sequence_length,
     learn_means=learn_means,
     learn_covariances=learn_covariances,
-    initial_means=means,
-    initial_covariances=covariances,
+    initial_means=None,
+    initial_covariances=None,
     n_layers_inference=n_layers_inference,
     n_layers_model=n_layers_model,
     n_units_inference=n_units_inference,
@@ -102,7 +95,7 @@ prediction_dataset = meg_data.prediction_dataset(sequence_length, batch_size)
 
 # Train the model
 print("Training model")
-history = model.fit(training_dataset, epochs=n_epochs, verbose=0, use_tqdm=True)
+history = model.fit(training_dataset, epochs=n_epochs)
 
 # Inferred covariance matrices
 int_means, inf_cov = model.get_means_covariances()
@@ -113,12 +106,17 @@ inf_stc = model.predict_states(prediction_dataset)
 inf_stc = inf_stc.argmax(axis=1)
 inf_stc = array_ops.get_one_hot(inf_stc)
 
+# State time course from HMM
+hmm = data.OSL_HMM("/well/woolrich/shared/vrad/hmm_fits/ten_subjects_1/hmm.mat")
+hmm_stc = hmm.viterbi_path
+
 # Find correspondance between state time courses
-matched_stc, matched_inf_stc = array_ops.match_states(hmm.viterbi_path, inf_stc)
+matched_stc, matched_inf_stc = array_ops.match_states(hmm_stc, inf_stc)
 # plotting.compare_state_data(matched_stc, matched_inf_stc, filename="compare.png")
 
 # Dice coefficient
-print("Dice coefficient:", metrics.dice_coefficient(matched_stc, matched_inf_stc))
+dc = metrics.dice_coefficient(matched_stc, matched_inf_stc)
+print(f"Dice coefficient: {dc}")
 
 # Free energy
 free_energy, ll_loss, kl_loss = model.free_energy(prediction_dataset, return_all=True)

src/vrad/inference/functions.py
@@ -41,36 +41,12 @@ def cholesky_factor(full_matrix):
     return cholesky_factor
 
 
-def matrix_sqrt_3d(matrix):
-    """A wrapper function for `scipy.linalg.sqrtm`.
-
-    SciPy's matrix square root function only works on [N x N] 2D matrices. This
-    function provides a simple solution for performing this operation on a stack of
-    [N x N] 2D arrays.
-
-    Parameters
-    ----------
-    matrix : numpy.ndarray
-        [M x N x N] matrix.
-
-    Returns
-    -------
-    matrix_sqrt : numpy.ndarray
-        A stack of matrix square roots of the same dimensions as `matrix` ([M x N x N])
-    """
-    if matrix.ndim != 3 or matrix.shape[1] != matrix.shape[2]:
-        raise ValueError("Only accepts matrices with dimensions M x N x N")
-    return_matrix = np.empty_like(matrix)
-    for index, layer in enumerate(matrix):
-        return_matrix[index] = scipy.linalg.sqrtm(layer)
-    return return_matrix
-
-
 @tf.function
-def normalize_covariances(covariances):
-    """Normalise covariance matrix based on its trace.
+def trace_normalize(matrices):
+    """Normalise a matrix based on its trace.
 
-    The trace of `covariance` is taken. All values are then divided by it.
+    The trace of each matrix in 'matrices' is taken. All values are then
+    divided by it.
 
     Parameters
     ----------
@@ -82,36 +58,7 @@ def normalize_covariances(covariances):
     normalized_covariances : tf.Tensor
         Tensor of the form [M x N x N]
     """
-    normalization = tf.reduce_sum(tf.linalg.diag_part(covariances), axis=1)[
+    normalization = tf.reduce_sum(tf.linalg.diag_part(matrices), axis=1)[
         ..., tf.newaxis, tf.newaxis
     ]
-    normalized_covariances = covariances / normalization
-    return normalized_covariances
-
-
-def full_covariances(means, covariances):
-    """Calculate full covariance.
-
-    Given a set of covariances and means, calculate the full covariance matrices.
-
-    Parameters
-    ----------
-    means : numpy.ndarray
-        Matrix of means of dimensions [n_states x n_channels].
-    covariances : numpy.ndarray
-        Matrix of covariances of dimensions [n_states x n_channels x n_channels].
-
-    Returns
-    -------
-    full_covariances : numpy.ndarray
-        Normalised full covariance matrices of dimensions [n_states x n_channels
-        x n_channels].
-    """
-    means = means[:, np.newaxis, :]
-    full_covariances = (means @ means.transpose(0, 2, 1)) + covariances
-    return full_covariances
-
-
-def softplus(x):
-    """Softplus function"""
-    return np.log(1 + np.exp(x))
+    return matrices.shape[1] * matrices / normalization

src/vrad/inference/layers.py
@@ -2,12 +2,13 @@
 
 """
 
+import logging
 from typing import Union
 
 import numpy as np
 import tensorflow as tf
-import tensorflow.keras.layers as layers
 import tensorflow_probability as tfp
+from tensorflow.keras import layers
 from tensorflow.keras import backend as K
 from tensorflow.keras.activations import softmax, softplus
 from tensorflow.python.keras.backend import stop_gradient
@@ -15,13 +16,15 @@ from vrad.inference.functions import (
     cholesky_factor,
     cholesky_factor_to_full_matrix,
     is_symmetric,
-    normalize_covariances,
+    trace_normalize,
 )
 from vrad.inference.initializers import (
     CholeskyCovariancesInitializer,
     Identity3D,
     MeansInitializer,
 )
+
+_logger = logging.getLogger("VRAD")
 
 
 class ReparameterizationLayer(layers.Layer):
@@ -78,6 +81,10 @@ class MultivariateNormalLayer(layers.Layer):
         self.burnin = tf.Variable(False, trainable=False)
 
         if initial_covariances is not None:
+            # Normalise the covariances if required
+            if normalize_covariances:
+                initial_covariances = trace_normalize(initial_covariances)
+
             # If the matrix is symmetric we assume it's the full covariance matrix
             # WARNING: diagonal matrices are assumed to be the full covariance matrix
             if is_symmetric(initial_covariances):
@@ -138,7 +145,7 @@ class MultivariateNormalLayer(layers.Layer):
         self.covariances = tf.cond(self.burnin, no_grad, with_grad)
 
         if self.normalize_covariances:
-            self.covariances = normalize_covariances(self.covariances)
+            self.covariances = trace_normalize(self.covariances)
 
         return [self.means, self.covariances]
 
@@ -225,7 +232,13 @@ class MixMeansCovsLayer(layers.Layer):
         self.n_states = n_states
         self.n_channels = n_channels
         self.alpha_xform = alpha_xform
-        self.learn_alpha_scaling = learn_alpha_scaling
+        self.learn_alpha_scaling = learn_alpha_scaling and alpha_xform is not "softplus"
+
+        if learn_alpha_scaling and alpha_xform == "softplus":
+            _logger.warning(
+                "Warning: learn_alpha_scaling set to False because "
+                + "alpha_xform=softplus is being used."
+            )
 
     def build(self, input_shape):
         # Initialise such that softplus(alpha_scaling) = 1