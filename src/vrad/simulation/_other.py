904c52ccc90bb99348b74aec35fbb8dd5e73cf70
Evan <evanr70@gmail.com>
2020-08-04 10:20:44 +0100

    Added VRADSimulation to simulate from covariances and a state time course.

--- src/vrad/simulation/_other.py
+++ src/vrad/simulation/_other.py
@@ -106,3 +106,23 @@ class HiddenSemiMarkovSimulation(Simulation):
         logging.debug(f"one_hot_alpha_sim.shape = {one_hot_alpha_sim.shape}")
 
         return one_hot_alpha_sim
+
+
+class VRADSimulation(Simulation):
+    """Simulate a dataset from the covariances and state time course of a model."""
+
+    def __init__(self, covariances, state_time_course):
+        super().__init__(
+            n_samples=state_time_course.shape[0],
+            n_channels=covariances.shape[-1],
+            n_states=covariances.shape[0],
+            sim_varying_means=False,
+            covariances=covariances,
+            observation_error=0.0,
+            random_covariance_weights=False,
+            simulate=False,
+        )
+        self.state_time_course = state_time_course
+
+    def generate_states(self):
+        return self.state_time_course
