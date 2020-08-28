2477340c0209e0be59c4c12d6a32adb4f3187f56
Chetan Gohil <chetan.gohil@psych.ox.ac.uk>
2020-08-28 15:53:14 +0100

    Added function to save nii files containing spatial maps.

--- src/vrad/analysis/maps.py
+++ src/vrad/analysis/maps.py
@@ -2,6 +2,7 @@
 
 """
 
+import nibabel as nib
 import numpy as np
 from vrad.analysis.functions import validate_array
 
@@ -65,3 +66,60 @@ def state_maps(power_spectra, coherences, components):
     c[:, :, range(n_channels), range(n_channels)] = 1
 
     return p, c
+
+
+def save_nii_file(mask_file, parcellation_file, power_map, filename, component=0):
+    """Saves a NITFI file containing a map."""
+
+    # Add extension if it's not already there
+    if "nii" not in filename:
+        filename += ".nii.gz"
+
+    # Load the mask
+    mask = nib.load(mask_file)
+    mask_grid = mask.get_data()
+
+    # Flatten the mask
+    mask_grid = mask_grid.ravel(order="F")
+
+    # Get indices of non-zero elements, i.e. those which contain the brain
+    non_zero_voxels = mask_grid != 0
+
+    # Load the parcellation
+    parcellation = nib.load(parcellation_file)
+    parcellation_grid = parcellation.get_data()
+
+    # Number of parcels
+    n_parcels = parcellation.shape[-1]
+
+    # Make a 1D array of voxel weights for each channel
+    voxels = parcellation_grid.reshape(-1, n_parcels, order="F")[non_zero_voxels]
+
+    # Number of voxels
+    n_voxels = voxels.shape[0]
+
+    # Normalise the voxels to have comparable weights
+    voxels /= voxels.max(axis=0)[np.newaxis, ...]
+
+    # Number of components, states, channels
+    n_components, n_states, n_channels, n_channels = power_map.shape
+
+    # Generate spatial map
+    spatial_map = np.empty([n_voxels, n_states])
+    for i in range(n_states):
+        spatial_map[:, i] = voxels @ np.diag(np.squeeze(power_map[component, i]))
+
+    # Subtract mean power across states
+    spatial_map -= np.mean(spatial_map, axis=1)[..., np.newaxis]
+
+    # Convert spatial map into a grid
+    spatial_map_grid = np.zeros([mask_grid.shape[0], n_states])
+    spatial_map_grid[non_zero_voxels] = spatial_map
+    spatial_map_grid = spatial_map_grid.reshape(
+        mask.shape[0], mask.shape[1], mask.shape[2], n_states, order="F"
+    )
+
+    # Save as nii file
+    print(f"Saving {filename}")
+    nii_file = nib.Nifti1Image(spatial_map_grid, mask.affine, mask.header)
+    nib.save(nii_file, filename)
