Fixed the factor 0.5 discrepency in the Sampling and KL divergence layer.

src/vrad/models/layers.py
@@ -90,8 +90,7 @@ class SampleNormalDistributionLayer(layers.Layer):
 
     def call(self, inputs, **kwargs):
         mu, log_sigma = inputs
-        # TODO: should really be scale=tf.exp(log_sigma)
-        N = tfp.distributions.Normal(loc=mu, scale=tf.exp(0.5 * log_sigma))
+        N = tfp.distributions.Normal(loc=mu, scale=tf.exp(log_sigma))
         return N.sample()
 
     def compute_output_shape(self, input_shape):