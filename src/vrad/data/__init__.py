0f11a97d855563b57a134c4ffe2efb9373c39ba1
Chetan Gohil <chetan.gohil@psych.ox.ac.uk>
2020-10-03 13:35:04 +0100

    Removed the Subject class.

--- src/vrad/data/__init__.py
+++ src/vrad/data/__init__.py
@@ -1,6 +1,5 @@
 from vrad.data._base import Data
 from vrad.data._preprocessed import PreprocessedData
 from vrad.data._osl import OSL_HMM
-from vrad.data._subject import Subject
 
-__all__ = ["Data", "PreprocessedData", "OSL_HMM", "Subject"]
+__all__ = ["Data", "PreprocessedData", "OSL_HMM"]
