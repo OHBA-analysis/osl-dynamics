74871266f6ef515854b9ff65bd4040dd6d227f78
Evan <evanr70@gmail.com>
2021-01-19 14:12:34 +0000

    Added random covariance sampling example.

---
+++ examples/random_covariance_sample.py
@@ -0,0 +1,60 @@
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import vrad
+from vrad.data import Data
+from vrad.inference.states import covariance_sample, match_covariances
+from vrad.simulation import HSMMSimulation
+from vrad.utils import plotting
+
+n_samples = 128000
+observation_error = 0.2
+gamma_shape = 10
+gamma_scale = 5
+
+n_states = 5
+
+covs = np.load(str(Path(vrad.example_files) / "hmm_cov.npy"))
+
+sim = HSMMSimulation(
+    n_samples=n_samples,
+    gamma_shape=gamma_shape,
+    gamma_scale=gamma_scale,
+    zero_means=True,
+    covariances=covs,
+    random_seed=123,
+)
+
+data = Data(sim)
+
+prepared = data[0]
+
+full_covariance = np.cov(prepared.T)
+no_diag = plotting.mean_diagonal(full_covariance)
+
+plt.matshow(no_diag)
+plt.suptitle("Data covariance (diagonal removed)")
+plt.show()
+
+
+sample_covs = covariance_sample(
+    data=prepared,
+    segment_length=[50 * i for i in range(1, 5)],
+    n_segments=100,
+    n_clusters=5,
+)
+
+covs, sample_covs = match_covariances(covs, sample_covs)
+
+plotting.plot_matrices(sample_covs, main_title="GT covariances")
+plotting.plot_matrices(
+    [plotting.mean_diagonal(cov) for cov in sample_covs],
+    main_title="GT covariances (diagonal removed)",
+)
+
+plotting.plot_matrices(covs, main_title="Sampled covariances")
+plotting.plot_matrices(
+    [plotting.mean_diagonal(cov) for cov in covs],
+    main_title="Sampled covariances (diagonal removed)",
+)
