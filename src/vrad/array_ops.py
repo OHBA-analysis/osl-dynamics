d3c9029d74be28865c8ccf0d00649846b24a635e
Evan <evanr70@gmail.com>
2021-02-13 12:41:41 +0000

    added sliding window function

--- src/vrad/array_ops.py
+++ src/vrad/array_ops.py
@@ -177,3 +177,48 @@ def trace_weights(covariance_matrices: Union[np.ndarray, List[np.ndarray]]):
     weights = np.trace(covariance_matrices, axis1=1, axis2=2)
     normalized_weights = weights / weights.max()
     return normalized_weights
+
+
+def sliding_window_view(x, window_shape, axis=None, *, subok=False, writeable=False):
+    """Create a sliding window over an array in arbitrary dimensions.
+
+    Unceremoniously ripped from numpy 1.20, np.lib.stride_tricks.sliding_window_view.
+    """
+    window_shape = tuple(window_shape) if np.iterable(window_shape) else (window_shape,)
+    # first convert input to array, possibly keeping subclass
+    x = np.array(x, copy=False, subok=subok)
+
+    window_shape_array = np.array(window_shape)
+    if np.any(window_shape_array < 0):
+        raise ValueError("`window_shape` cannot contain negative values")
+
+    if axis is None:
+        axis = tuple(range(x.ndim))
+        if len(window_shape) != len(axis):
+            raise ValueError(
+                f"Since axis is `None`, must provide "
+                f"window_shape for all dimensions of `x`; "
+                f"got {len(window_shape)} window_shape elements "
+                f"and `x.ndim` is {x.ndim}."
+            )
+    else:
+        axis = np.core.numeric.normalize_axis_tuple(axis, x.ndim, allow_duplicate=True)
+        if len(window_shape) != len(axis):
+            raise ValueError(
+                f"Must provide matching length window_shape and "
+                f"axis; got {len(window_shape)} window_shape "
+                f"elements and {len(axis)} axes elements."
+            )
+
+    out_strides = x.strides + tuple(x.strides[ax] for ax in axis)
+
+    # note: same axis can be windowed repeatedly
+    x_shape_trimmed = list(x.shape)
+    for ax, dim in zip(axis, window_shape):
+        if x_shape_trimmed[ax] < dim:
+            raise ValueError("window shape cannot be larger than input array shape")
+        x_shape_trimmed[ax] -= dim - 1
+    out_shape = tuple(x_shape_trimmed) + window_shape
+    return np.lib.stride_tricks.as_strided(
+        x, strides=out_strides, shape=out_shape, subok=subok, writeable=writeable
+    )
