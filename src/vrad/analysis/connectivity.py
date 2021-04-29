2f7567681060dbbe2ea29e63dbeb2e05b74fb077
Evan <evanr70@gmail.com>
2021-04-29 10:27:41 +0100

    added function to create visbrain connectivity selection using stds from mean of edge strengths. Ignores diagonal components

--- src/vrad/analysis/connectivity.py
+++ src/vrad/analysis/connectivity.py
@@ -1,6 +1,19 @@
+import numpy as np
 from visbrain.objects import BrainObj, ConnectObj, SceneObj
 from vrad.analysis import std_masks
 from vrad.utils.parcellation import Parcellation
+
+
+def exclude_by_sigma(edges, sigma=1):
+    edges = edges.copy()
+    np.fill_diagonal(edges, np.nan)
+
+    mean = edges[~np.isnan(edges)].mean()
+    std = edges[~np.isnan(edges)].std()
+
+    np.fill_diagonal(edges, mean)
+    selection = (edges >= (mean + sigma * std)) & (edges <= (mean - sigma * std))
+    return selection
 
 
 def plot_connectivity(edges, parcellation, *, inflation=0, selection=None):
