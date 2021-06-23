6e77bc4e5d1d8c4d8eab3e8058b2b869fa5464de
Evan <evanr70@gmail.com>
2021-06-23 11:13:41 +0100

    Included nilearn connectivity functions

--- src/vrad/analysis/connectivity.py
+++ src/vrad/analysis/connectivity.py
@@ -1,4 +1,5 @@
 import numpy as np
+from nilearn.plotting import plot_connectome
 from visbrain.objects import BrainObj, ConnectObj, SceneObj
 from vrad.files import mask
 from vrad.utils.parcellation import Parcellation
@@ -16,7 +17,34 @@ def exclude_by_sigma(edges, sigma=1):
     return selection
 
 
-def plot_connectivity(edges, parcellation, *, inflation=0, selection=None):
+def std_filter(arr, sigma=0.95):
+    copy = arr.copy()
+
+    mean = copy.mean()
+    std = copy.std()
+
+    low_pass = mean - sigma * std
+    high_pass = mean + sigma * std
+
+    copy[(copy > low_pass) & (copy < high_pass)] = 0
+    return copy
+
+
+def make_symmetric(arr, upper=True):
+    copy = arr.copy()
+    index = np.tril_indices(copy.shape[-1], -1)
+    copy[..., index[0], index[1]] = np.inf
+    return np.minimum(copy, np.swapaxes(copy, -2, -1))
+
+
+def plot_connectivity(edges, parcellation, sigma=0, **kwargs):
+    filtered = std_filter(edges, sigma)
+    if not isinstance(parcellation, Parcellation):
+        parcellation = Parcellation(parcellation)
+    plot_connectome(filtered, parcellation.roi_centers(), **kwargs)
+
+
+def visbrain_plot_connectivity(edges, parcellation, *, inflation=0, selection=None):
     if isinstance(parcellation, str):
         parcellation = Parcellation(parcellation)
     nodes = parcellation.roi_centers()
