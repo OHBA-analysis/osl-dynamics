c385c95e23cd88460f1e00faa329e1cdf0f92bfe
Chetan Gohil <chetan.gohil@psych.ox.ac.uk>
2021-07-27 18:16:48 +0100

    Load files in the import.

--- src/vrad/files/scanner/__init__.py
+++ src/vrad/files/scanner/__init__.py
@@ -1,7 +1,8 @@
+import numpy as np
 from pathlib import Path
 
 path = Path(__file__).parent
 directory = str(path)
 
-ctf275_channel_names = str(path / "ctf275_channel_names.npy")
-neuromag306_channel_names = str(path / "neuromag306_channel_names.npy")
+ctf275_channel_names = np.load(path / "ctf275_channel_names.npy")
+neuromag306_channel_names = np.load(path / "neuromag306_channel_names.npy")
