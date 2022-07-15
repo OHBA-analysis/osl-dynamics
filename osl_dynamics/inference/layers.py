Fix: Correct scaling for the mode parameter regularisations.

osl_dynamics/inference/layers.py
@@ -319,6 +319,13 @@ class MeanVectorsLayer(layers.Layer):
     def call(self, inputs, **kwargs):
         if self.regularizer is not None:
             reg = self.regularizer(self.vectors)
+
+            # Calculate the the scaling on regularisation
+            batch_size = tf.cast(tf.shape(inputs)[0], tf.float32)
+            n_batches = self.regularizer.n_batches
+            scaling_factor = batch_size * n_batches
+            reg = reg / scaling_factor
+
             self.add_loss(reg)
             self.add_metric(reg, name=self.name)
         return self.vectors
@@ -410,6 +417,13 @@ class CovarianceMatricesLayer(layers.Layer):
         covariances = self.bijector(self.flattened_cholesky_factors)
         if self.regularizer is not None:
             reg = self.regularizer(covariances)
+
+            # Calculate the the scaling on regularisation
+            batch_size = tf.cast(tf.shape(inputs)[0], tf.float32)
+            n_batches = self.regularizer.n_batches
+            scaling_factor = batch_size * n_batches
+            reg = reg / scaling_factor
+
             self.add_loss(reg)
             self.add_metric(reg, name=self.name)
         return covariances
@@ -506,6 +520,13 @@ class CorrelationMatricesLayer(layers.Layer):
         correlations = self.bijector(self.flattened_cholesky_factors)
         if self.regularizer is not None:
             reg = self.regularizer(correlations)
+
+            # Calculate the the scaling on regularisation
+            batch_size = tf.cast(tf.shape(inputs)[0], tf.float32)
+            n_batches = self.regularizer.n_batches
+            scaling_factor = batch_size * n_batches
+            reg = reg / scaling_factor
+
             self.add_loss(reg)
             self.add_metric(reg, name=self.name)
         return correlations
@@ -584,10 +605,17 @@ class DiagonalMatricesLayer(layers.Layer):
         )
         self.built = True
 
-    def call(self, alpha, **kwargs):
+    def call(self, inputs, **kwargs):
         D = self.bijector(self.diagonals)
         if self.regularizer is not None:
             reg = self.regularizer(D)
+
+            # Calculate the the scaling on regularisation
+            batch_size = tf.cast(tf.shape(inputs)[0], tf.float32)
+            n_batches = self.regularizer.n_batches
+            scaling_factor = batch_size * n_batches
+            reg = reg / scaling_factor
+
             self.add_loss(reg)
             self.add_metric(reg, name=self.name)
         D = tf.linalg.diag(D)

osl_dynamics/inference/regularizers.py
@@ -17,12 +17,15 @@ class InverseWishart(regularizers.Regularizer):
     psi : np.ndarray
         Scale matrix. Must be a symmetric positive definite matrix.
         Shape must be (n_channels, n_channels).
+    n_batches : int
+        Number of batches in the data.
     """
 
-    def __init__(self, nu, psi, **kwargs):
+    def __init__(self, nu, psi, n_batches, **kwargs):
         super().__init__(**kwargs)
         self.nu = nu
         self.psi = psi
+        self.n_batches = n_batches
         self.n_channels = psi.shape[-1]
 
         # Validation
@@ -63,12 +66,15 @@ class MultivariateNormal(regularizers.Regularizer):
     sigma : np.ndarray
         2D numpy array of covariance matrix of the prior.
         Shape must be (n_channels, n_channels).
+    n_batches : int
+        Number of batches in the data.
     """
 
-    def __init__(self, mu, sigma, **kwargs):
+    def __init__(self, mu, sigma, n_batches, **kwargs):
         super().__init__(**kwargs)
         self.mu = mu
         self.sigma = sigma
+        self.n_batches = n_batches
 
         # Validation
         if self.mu.ndim != 1:
@@ -116,13 +122,15 @@ class MarginalInverseWishart(regularizers.Regularizer):
         Degrees of freedom. Must be greater than (n_channels - 1).
     n_channels : int
         Number of channels of the correlation matrices.
+    n_batches : int
+        Number of batches in the data.
     """
 
-    def __init__(self, nu, n_channels, **kwargs):
+    def __init__(self, nu, n_channels, n_batches, **kwargs):
         super().__init__(**kwargs)
         self.nu = nu
         self.n_channels = n_channels
-
+        self.n_batches = n_batches
         # Validation
         if not self.nu > self.n_channels - 1:
             raise ValueError("nu must be greater than (n_channels - 1).")
@@ -147,12 +155,15 @@ class LogNormal(regularizers.Regularizer):
     sigma : np.ndarray
         Sigma parameters of the log normal distribution.
         Shape is (n_channels,). All entries must be positive.
+    n_batches : int
+        Number of batches in the data.
     """
 
-    def __init__(self, mu, sigma, **kwargs):
+    def __init__(self, mu, sigma, n_batches, **kwargs):
         super().__init__(**kwargs)
         self.mu = mu
         self.sigma = sigma
+        self.n_batches = n_batches
 
         # Validation
         if self.mu.ndim != 1:

osl_dynamics/models/dynemo.py
@@ -226,13 +226,15 @@ class Model(VariationalInferenceModelBase):
         """
         dynemo_obs.set_covariances(self.model, covariances, update_initializer)
 
-    def set_means_regularizer(self, training_data=None, mu=None, sigma=None):
+    def set_means_regularizer(self, n_batches, training_data=None, mu=None, sigma=None):
         """Set the means vector regularizer.
 
         The regularization is equivalent to applying a multivariate normal prior.
 
         Parameters
         ----------
+        n_batches : int
+            Number of batches.
         training_data : osl_dynamics.data.Data
             Estimate mu and sigma using the training data instead of specifying it
             explicitly. If training_data is passed, diag(sigma)=((max - min) / 2)**2.
@@ -253,18 +255,22 @@ class Model(VariationalInferenceModelBase):
             mu = np.zeros(self.config.n_channels, dtype=np.float32)
 
         means_layer = self.model.get_layer("means")
-        means_layer.regularizer = regularizers.MultivariateNormal(mu, sigma)
+        means_layer.regularizer = regularizers.MultivariateNormal(mu, sigma, n_batches)
 
-    def set_covariances_regularizer(self, training_data=None, nu=None, psi=None):
+    def set_covariances_regularizer(
+        self, n_batches, training_data=None, nu=None, psi=None
+    ):
         """Set the covariance matrices regularizer.
 
         Parameters
         ----------
+        n_batches : int
+            Number of batches.
         training_data : osl_dynamics.data.Data
             Estimate nu and psi using the training data instead of specifying it
             explicitly. If training_data is passed, nu=n_channels - 1 + 0.1
-            and psi=1 / (max - min)
-        nu : int
+            and psi=diag(1 / (max - min)).
+        nu : float
             Degrees of freedom of the prior.
         psi : np.ndarray
             Scale matrix of the prior. Shape must be (n_channels, n_channels).
@@ -280,9 +286,9 @@ class Model(VariationalInferenceModelBase):
             psi = np.diag(1 / range_)
 
         covs_layer = self.model.get_layer("covs")
-        covs_layer.regularizer = regularizers.InverseWishart(nu, psi)
+        covs_layer.regularizer = regularizers.InverseWishart(nu, psi, n_batches)
 
-    def set_means_covariances_regularizer(self, training_data):
+    def set_regularizers(self, n_batches, training_data):
         """Set the means and covariances regularizer based on the training data.
 
         A multivariate normal prior is applied to the mean vectors with mu = 0,
@@ -291,14 +297,16 @@ class Model(VariationalInferenceModelBase):
 
         Parameters
         ----------
+        n_batches : int
+            Number of batches.
         training_data : osl_dynamics.data.Data
             Training dataset.
         """
         if self.config.learn_means:
-            self.set_means_regularizer(training_data)
+            self.set_means_regularizer(n_batches, training_data)
 
         if self.config.learn_covariances:
-            self.set_covariances_regularizer(training_data)
+            self.set_covariances_regularizer(n_batches, training_data)
 
     def sample_alpha(self, n_samples, theta_norm=None):
         """Uses the model RNN to sample mode mixing factors, alpha.

osl_dynamics/models/mdynemo.py
@@ -230,13 +230,15 @@ class Model(VariationalInferenceModelBase):
         """
         mdynemo_obs.set_means_stds_fcs(self.model, means, stds, fcs, update_initializer)
 
-    def set_means_regularizer(self, training_data=None, mu=None, sigma=None):
+    def set_means_regularizer(self, n_batches, training_data=None, mu=None, sigma=None):
         """Set the means vector regularizer.
 
         The regularization is equivalent to applying a multivariate normal prior.
 
         Parameters
         ----------
+        n_batches : int
+            Number of batches.
         training_data : osl_dynamics.data.Data
             Estimate mu and sigma using the training data instead of specifying it
             explicitly. If training_data is passed, diag(sigma)=((max - min) / 2)**2.
@@ -257,9 +259,9 @@ class Model(VariationalInferenceModelBase):
             mu = np.zeros(self.config.n_channels, dtype=np.float32)
 
         means_layer = self.model.get_layer("means")
-        means_layer.regularizer = regularizers.MultivariateNormal(mu, sigma)
+        means_layer.regularizer = regularizers.MultivariateNormal(mu, sigma, n_batches)
 
-    def set_stds_regularizer(self, training_data=None, mu=None, sigma=None):
+    def set_stds_regularizer(self, n_batches, training_data=None, mu=None, sigma=None):
         """Set the stds vector regularizer.
 
         The regularization is equivalent to applying independent log normal priors
@@ -267,6 +269,8 @@ class Model(VariationalInferenceModelBase):
 
         Parameters
         ----------
+        n_batches : int
+            Number of batches.
         training_data : osl_dynamics.data.Data
             Estimate mu and sigma using the training data instead of specifying it
             explicitly. If training_data is passed, mu=0 and
@@ -287,9 +291,9 @@ class Model(VariationalInferenceModelBase):
             sigma = np.sqrt(np.log(2 * range_))
 
         stds_layer = self.model.get_layer("stds")
-        stds_layer.regularizer = regularizers.LogNormal(mu, sigma)
+        stds_layer.regularizer = regularizers.LogNormal(mu, sigma, n_batches)
 
-    def set_fcs_regularizer(self, training_data=None, nu=None):
+    def set_fcs_regularizer(self, n_batches, training_data=None, nu=None):
         """Set the fcs matrix regularizer.
 
         The regularization is equivalent to applying an inverse Wishart prior
@@ -297,6 +301,8 @@ class Model(VariationalInferenceModelBase):
 
         Parameters
         ----------
+        n_batches : int
+            Number of batches.
         training_data : osl_dynamics.data.Data
             Estimate nu using the training data. If training_data is passed,
             nu = n_channels - 1 + 0.1.
@@ -312,10 +318,12 @@ class Model(VariationalInferenceModelBase):
 
         fcs_layer = self.model.get_layer("fcs")
         fcs_layer.regularizer = regularizers.MarginalInverseWishart(
-            nu, self.config.n_channels
+            nu,
+            self.config.n_channels,
+            n_batches,
         )
 
-    def set_regularizers(self, training_data):
+    def set_regularizers(self, n_batches, training_data):
         """Set the regularizers of means, stds and fcs based on the training data.
 
         A multivariate normal prior is applied to the mean vectors with mu=0,
@@ -325,17 +333,19 @@ class Model(VariationalInferenceModelBase):
 
         Parameters
         ----------
+        n_batches : int
+            Number of batches.
         training_data : osl_dynamics.data.Data
             Training dataset.
         """
         if self.config.learn_means:
-            self.set_means_regularizer(training_data)
+            self.set_means_regularizer(n_batches, training_data)
 
         if self.config.learn_stds:
-            self.set_stds_regularizer(training_data)
+            self.set_stds_regularizer(n_batches, training_data)
 
         if self.config.learn_fcs:
-            self.set_fcs_regularizer(training_data)
+            self.set_fcs_regularizer(n_batches, training_data)
 
     def sample_time_courses(self, n_samples: int):
         """Uses the model RNN to sample mode mixing factors, alpha and gamma.