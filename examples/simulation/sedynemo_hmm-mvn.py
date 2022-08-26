f4a1d6118f9905cdaba9b5aad65f53346402bfaa
Rukuang Huang <rukuang.huang@jesus.ox.ac.uk>
2022-08-26 14:09:44 +0100

    Fix: Updated SE-DyNeMo example.

--- examples/simulation/sedynemo_hmm-mvn.py
+++ examples/simulation/sedynemo_hmm-mvn.py
@@ -35,7 +35,7 @@ config = Config(
     kl_annealing_sharpness=10,
     n_kl_annealing_epochs=100,
     batch_size=256,
-    learning_rate=0.01,
+    learning_rate=0.005,
     n_epochs=200,
     multi_gpu=False,
 )
@@ -51,7 +51,7 @@ sim = simulation.MSubj_HMM_MVN(
     n_states=config.n_modes,
     n_channels=config.n_channels,
     n_subjects=config.n_subjects,
-    n_groups=2,
+    n_groups=1,
     stay_prob=0.9,
     random_seed=123,
 )
@@ -61,7 +61,7 @@ training_data = data.Data([mtc for mtc in sim.time_series])
 # Bayesian about subject deviations?
 config.dev_bayesian = True
 config.learn_dev_mod_sigma = True
-config.initial_dev_mod_sigma = np.sqrt(0.5 / 1e-3)
+config.initial_dev_mod_sigma = 100
 
 # Build model
 model = Model(config)
@@ -108,7 +108,21 @@ plotting.plot_scatter(
     filename="subject_embeddings.png",
 )
 
+
 # Look at the ground truth group assignment
-covariances_assigned_groups = sim.obs_mod.covariances_assigned_groups
-print("group 0: ", np.where(covariances_assigned_groups == 0)[0])
-print("group 1: ", np.where(covariances_assigned_groups == 1)[0])
+assigned_groups = np.empty([config.n_subjects, 2])
+assigned_groups[:, 0] = (
+    sim.obs_mod.means_assigned_groups
+    if sim.obs_mod.means_assigned_groups is not None
+    else 0
+)
+assigned_groups[:, 1] = (
+    sim.obs_mod.covariances_assigned_groups
+    if sim.obs_mod.covariances_assigned_groups is not None
+    else 0
+)
+
+unique_groups = np.unique(assigned_groups, axis=0)
+for i in range(unique_groups.shape[0]):
+    print("group", unique_groups[i], ":")
+    print(np.where(np.all(assigned_groups == unique_groups[i], axis=1))[0])
