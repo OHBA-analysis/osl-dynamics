ee2db0def2a813da08ba3534a67891bd603d0f98
evanr70 <evanr70@gmail.com>
2023-02-17 13:45:15 +0000

    feat: added package level logging config

--- osl_dynamics/__init__.py
+++ osl_dynamics/__init__.py
@@ -1,4 +1,5 @@
 from pkg_resources import DistributionNotFound, get_distribution
+import logging
 
 try:
     __version__ = get_distribution("osl-dynamics").version
@@ -6,3 +7,15 @@ except DistributionNotFound:
     __version__ = "unknown"
 finally:
     del get_distribution, DistributionNotFound
+
+# Configure logging for package
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s %(name)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO,
+)
+
+logger = logging.getLogger("osl-dynamics")
+logger.debug("Version %s", __version__)
+
+del logger, logging
