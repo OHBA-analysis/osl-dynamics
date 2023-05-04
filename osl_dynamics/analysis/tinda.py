519591bc310ccee0b590e13c0a2c5f338c6d1b80
matsvanes <mats.vanes@psych.ox.ac.uk>
2023-05-04 15:32:19 +0100

    improve readability

--- osl_dynamics/analysis/tinda.py
+++ osl_dynamics/analysis/tinda.py
@@ -6,6 +6,7 @@ it is originally intended to use it on an HMM state time course to
 ask questions like what is the density of state j in the first and 
 second part of the interval between visits to state i
 """
+# Author: Mats van Es <mats.vanes@psych.ox.ac.uk>
 
 from itertools import permutations
 
@@ -65,31 +66,33 @@ def split_intervals(intervals, nbin=2):
     divided_intervals = []
     bin_sizes = []
     drop_mask = np.zeros(len(intervals))
-    for i, (start, end) in enumerate(intervals):
-        n_samples = end - start
-        bin_size = n_samples // nbin
-        if bin_size > 0:
-            remainder = n_samples % nbin
-            bins = []
-            bin_start = start
-            for i in range(nbin):
-                bin_end = bin_start + bin_size
-                bins.append((bin_start, bin_end))
-                if remainder > 0:
-                    if remainder == nbin - 1:
-                        bin_end += 1
-                    elif i == (nbin //2 - 1):
-                        bin_end += remainder
-                bin_start = bin_end
-            divided_intervals.append(bins)
-            bin_sizes.append(bin_size)
-        else:
+    for i, (interval_start, interval_end) in enumerate(intervals):
+        n_samples = interval_end - interval_start
+        if n_samples < nbin:
             drop_mask[i] = 1
+            continue
+
+        bin_size = n_samples // nbin
+        remainder = n_samples % nbin
+        bins = []
+        bin_start = interval_start
+        for i in range(nbin):
+            bin_end = bin_start + bin_size
+            bins.append((bin_start, bin_end))
+            if remainder > 0:
+                if remainder == nbin - 1:
+                    bin_end += 1
+                elif i == (nbin //2 - 1):
+                    bin_end += remainder
+            bin_start = bin_end
+        divided_intervals.append(bins)
+        bin_sizes.append(bin_size)
+
     return divided_intervals, bin_sizes, drop_mask
 
 
 def split_interval_duration(durations, interval_range=None, mode="sample", sfreq=None):
-    """Split interval durations into bins based on their duration
+    """Split interval durations into bins based on their duration.
 
     Parameters
     ----------
@@ -132,8 +135,8 @@ def split_interval_duration(durations, interval_range=None, mode="sample", sfreq
 
 
 def compute_fo_stats(tc_sec, divided_intervals, interval_mask=None, return_all_intervals=False):
-    """Compute weighted averages (weighted by interval duration and mean occurrence), 
-        and sums, of time courses in each interval
+    """Compute weighted averages of time courses in each interval.
+    
     Parameters
     ----------
     tc_sec: array_like
@@ -145,6 +148,7 @@ def compute_fo_stats(tc_sec, divided_intervals, interval_mask=None, return_all_i
         array of zeros and ones indicating whether the interval was in the bin
     return_all_intervals: bool
         whether to return the density/sum of all intervals in addition to the interval averages/sums
+    
     Returns
     -------
     interval_weighted_avg: array_like
@@ -160,11 +164,13 @@ def compute_fo_stats(tc_sec, divided_intervals, interval_mask=None, return_all_i
     """
     if interval_mask is None:
         interval_mask  = [np.ones(len(divided_intervals))]
+
     if len(divided_intervals[0])==2: # this corresponds to the matlab code but only works for two bins
         # and I think it's less principled than the code below
         intervals=[]
         for interval in divided_intervals:
             intervals.append([interval[0][0], interval[-1][-1]])
+        
         interval_weighted_avg = np.zeros((tc_sec.shape[1], 2, len(interval_mask)))  
         interval_sum = np.zeros((tc_sec.shape[1], 2, len(interval_mask)))  
         tempto=[]
@@ -173,11 +179,13 @@ def compute_fo_stats(tc_sec, divided_intervals, interval_mask=None, return_all_i
             d = int(np.floor((np.diff(i)-1)/2))
             tempaway.append(tc_sec[i[0]:i[0]+d+1,:])
             tempto.append(tc_sec[i[1]-d-1:i[1],:])
+
         interval_weighted_avg_all=[]
         interval_sum_all=[]
         for j, mask in enumerate(interval_mask):
             tempto_flat=np.concatenate([tempto[k] for k in np.where(mask==1)[0]])
             tempaway_flat=np.concatenate([tempaway[k] for k in np.where(mask==1)[0]])
+
             interval_weighted_avg[:,0,j] = np.mean(tempaway_flat,axis=0)
             interval_weighted_avg[:,1,j] = np.mean(tempto_flat,axis=0)
             interval_sum[:,0,j] = np.sum(tempaway_flat,axis=0)
@@ -192,10 +200,12 @@ def compute_fo_stats(tc_sec, divided_intervals, interval_mask=None, return_all_i
             for j, (start, end) in enumerate(interval):
                 interval_sum[:,j,i] = np.sum(tc_sec[start:end,:], axis=0)
             interval_weighted_avg[:,:,i] = interval_sum[:,:,i] / (end - start)
+
         interval_weighted_avg_all = [interval_weighted_avg[:,:,interval_selection==1] for interval_selection in interval_mask]
         interval_sum_all = [interval_sum[:,:,interval_selection==1] for interval_selection in interval_mask]
         interval_weighted_avg = np.stack([weighted_avg.mean(axis=-1) for weighted_avg in interval_weighted_avg_all], axis=-1)
         interval_sum = np.stack([int_sum.mean(axis=-1) for int_sum in interval_sum_all], axis=-1)
+
     if return_all_intervals:
         return interval_weighted_avg, interval_sum, interval_weighted_avg_all, interval_sum_all
     else:
@@ -204,6 +214,7 @@ def compute_fo_stats(tc_sec, divided_intervals, interval_mask=None, return_all_i
 
 def collate_stats(stats, field, all_to_all=False, ignore_elements=[]):
     """Collate stats across states
+
     Parameters
     ----------
     stats: list
@@ -217,6 +228,7 @@ def collate_stats(stats, field, all_to_all=False, ignore_elements=[]):
         2 dimensions are not n_states x n_states)
     ignore_elements: list
         list of states to ignore (i.e. because they don't contain binary events)
+    
     Returns
     -------
     collated_stat: array_like
@@ -232,6 +244,7 @@ def collate_stats(stats, field, all_to_all=False, ignore_elements=[]):
             collated_stat = np.full((2*[num_states]+list(shp[1:])), np.nan)
         else:
             collated_stat = np.full(([num_states]+list(shp)), np.nan)
+
         for i in range(num_states):
             if i in ignore_elements: # if intervals are not binary, keep a row of nans
                 continue
@@ -239,13 +252,16 @@ def collate_stats(stats, field, all_to_all=False, ignore_elements=[]):
                 collated_stat[i, np.arange(num_states) != i, ...] = stats[i][field]
             else:
                 collated_stat[i] =  stats[i][field]
+
     else:
         collated_stat = stats[0][field]
+
     return collated_stat
 
 
 def tinda(tc, density_of=None, nbin=2, interval_mode=None, interval_range=None, sfreq=None, return_all_intervals=False):
-    """Compute time-in-state density and sum for each interval
+    """Compute time-in-state density and sum for each interval.
+
     Parameters
     ---------- 
     tc: array_like
@@ -270,6 +286,7 @@ def tinda(tc, density_of=None, nbin=2, interval_mode=None, interval_range=None, 
         whether to return the density/sum of all intervals in addition to the interval averages/sums
         If True, will return a list of arrays in stats[i]['all_interval_wavgs'/'all_interval_sums'], 
         each corresponding to an interval range
+
     Returns
     -------
     fo_density: array_like
@@ -300,39 +317,51 @@ def tinda(tc, density_of=None, nbin=2, interval_mode=None, interval_range=None, 
             fo_density_tmp, fo_sum_tmp, stats = zip(*[tinda(itc, density_of[ix], nbin, interval_mode, interval_range, sfreq, return_all_intervals) for ix, itc in enumerate(tc)])
         fo_density = np.stack(fo_density_tmp, axis=-1)
         fo_sum = np.stack(fo_sum_tmp, axis=-1)
+
     else:
         stats=[]
         dim = tc.shape
         ignore_elements = []
+
         for i in range(dim[1]):
             itc_prim = tc[:, i]
             if not np.array_equal(itc_prim, itc_prim.astype(int)): # if not binary (i.e., intervals are not well difined)
                 stats.append(None)
                 ignore_elements.append(i)
+
             else:
                 if density_of is None:
                     itc_sec = tc[:, np.setdiff1d(range(dim[1]), i)] # we're doing density of all states in all states' intervals
                 else:
                     itc_sec = density_of
+                
+                # get interval info
                 intervals, durations = find_intervals(itc_prim)
                 divided_intervals, bin_sizes, dropped_intervals = split_intervals(intervals, nbin) # split intervals into nbin
                 durations = durations[dropped_intervals==0] # drop intervals that are too short to be split into nbin
                 interval_mask, interval_range_samples = split_interval_duration(durations, interval_range=interval_range, mode=interval_mode, sfreq=sfreq) # split intervals into interval_range (i.e.,
                 # to compute statistics of intervals with durations in a certain range)
+                
+                # compute time-in-state densities and sums in all intervals
                 interval_wavgs, interval_sums, all_interval_wavgs, all_interval_sums = compute_fo_stats(itc_sec, divided_intervals, interval_mask, return_all_intervals=return_all_intervals)
+                
                 # append stats
                 stats.append({"durations":durations,"intervals": intervals, "interval_wavgs":interval_wavgs, "interval_sums": interval_sums, 
                           "divided_intervals": divided_intervals, "bin_sizes": bin_sizes, "interval_range": interval_range_samples,
                           "all_interval_wavgs": all_interval_wavgs, "all_interval_sums": all_interval_sums})
+                
         # get a full matrix of FO densities and sums        
         fo_density = collate_stats(stats, 'interval_wavgs', all_to_all=density_of is None, ignore_elements=ignore_elements)
         fo_sum = collate_stats(stats, 'interval_sums', all_to_all=density_of is None, ignore_elements=ignore_elements)
+
     return fo_density, fo_sum, stats 
+
 
 #%% The following functions are used on the stats returned by tinda
 
 def optimise_sequence(fo_density, metric_to_use=0):
-    """
+    """Optimise the sequence to maximal circularity.
+
     This function reads in the mean pattern of differential fractional
     occupancy and computes the optimal display for a sequential circular
     plot visualization.
@@ -358,15 +387,22 @@ def optimise_sequence(fo_density, metric_to_use=0):
     bestseq : list
         list of best sequence of states to plot (in order of counterclockwise rotation)
     """
-    fo_density = np.squeeze(fo_density)
+    if len(fo_density.shape) == 5:
+        fo_density = np.squeeze(fo_density)
+
+    # compute different metrics to optimise    
     metric = []
     metric.append(np.mean(fo_density[:, :, 0, :] - fo_density[:, :, 1, :], axis=2))
     temp = (fo_density[:, :, 0, :] - fo_density[:, :, 1, :]) / np.mean(fo_density, axis=2)
     temp[np.isnan(temp)] = 0
     metric.append(np.mean(temp, axis=2))
     metric.append(np.mean(fo_density[:, :, 0, :] - fo_density[:, :, 1, :], axis=2) / np.mean(fo_density, axis=(2,3)))
+
     K = fo_density.shape[0]
+
+    # get all possible permutations of states
     myperms = np.array(list(permutations(range(1, K - 1), K - 2)))
+
     sequencemetric = np.zeros((len(myperms), 9, 3))
     for i2 in range(len(myperms)):
         if i2 % 10000 == 0:
@@ -380,10 +416,14 @@ def optimise_sequence(fo_density, metric_to_use=0):
             for i3 in range(K):
                 disttoplot_manual[manualorder[i3]] = np.exp(1j * (i3+1) / K * 2 * np.pi)
             angleplot = np.exp(1j * (np.angle(disttoplot_manual[:, np.newaxis]).T - np.angle(disttoplot_manual[:, np.newaxis])))
-            for i3 in range(3):
+            
+            # compute the metric
+            for i3 in range(len(metric)):
                 sequencemetric[i2, i, i3] = np.imag(np.nansum(np.nansum(angleplot * metric[i3])))         
+    
+    # find the permutation that maximizes the metric
     bestseq = []
-    for i in range(3):
+    for i in range(len(metric)):
         m1 = np.argmax(np.max(np.abs(sequencemetric[:, :, i]), axis=0))
         m = np.argmax(np.abs(sequencemetric[:, m1, i]))
         trueseqmetric = sequencemetric[m, m1, i]
@@ -393,7 +433,10 @@ def optimise_sequence(fo_density, metric_to_use=0):
         if trueseqmetric > 0:
             seq = [1] + list(reversed(seq[1:]))   
         bestseq.append(seq)
-    return [i-1 for i in bestseq[metric_to_use]]
+
+    # return the best sequence for the chosen metric (in order of counterclockwise rotation)    
+    bestseq = [i-1 for i in bestseq[metric_to_use]]
+    return bestseq
 
 
 
@@ -414,6 +457,7 @@ def plot_cycle(ordering, fo_density, sigpoints, newfigure=False, color_scheme=No
         array of size (K,3) color scheme to use for plotting (default is None)
         if None, will use the default color scheme from the matlab code
     """
+
     # Plot state network as circular diagram with arrows
     if color_scheme is None:
         color_scheme =  np.array([[0, 0, 1.0000], [1.0000,    0.3333,         0],    [1.0000,    0.6667,         0,],    [0.6667,    1.0000,    0.3333,],    [0.3333,    1.0000,    0.6667,],    [     0,    1.0000,    1.0000,],    [0.5529,    0.8275,    0.7804,],    [1.0000,    0.5000,    0.5000,],    [     0,    0.6667,    1.0000,],    [1.0000,    1.0000,         0,],    [0.7451,    0.7294,    0.8549,],    [0.6667,         0,         0,]])
@@ -421,26 +465,35 @@ def plot_cycle(ordering, fo_density, sigpoints, newfigure=False, color_scheme=No
         plt.figure(figsize=(6.02, 4.52), dpi=100)
     else:
         plt.gca()
-    fo_density = np.squeeze(fo_density) # squeeze in case there is still a interval_ranges dimension
-    mean_direction = (fo_density[:,:,0,:] - fo_density[:,:,1,:]).mean(axis=2)    
-    # now reorder the states to match the ordering:
+
+    K = len(ordering)
+    if len(fo_density.shape) == 5:    
+        fo_density = np.squeeze(fo_density) # squeeze in case there is still a interval_ranges dimension
+
+    # compute mean direction of arrows
+    mean_direction = np.squeeze((fo_density[:,:,0,:] - fo_density[:,:,1,:]).mean(axis=2))
+
+    # reorder the states to match the ordering:
     ordering = np.roll(ordering[::-1],1) # rotate ordering from clockwise to counter clockwise
     sigpoints = sigpoints[ordering][:,ordering]
     mean_direction = mean_direction[ordering][:,ordering]
-    K = len(ordering)
+
     # get the locations on the unit circle
     theta = np.arange(0, 2*np.pi, 2*np.pi/K)
-    x = np.roll(np.cos(theta),int(K/4))
+    x = np.roll(np.cos(theta),int(K/4)) # start from 12 o'clock
     y = np.roll(np.sin(theta),int(K-(K/4)))
     disttoplot_manual = np.stack([x,y]).T
+
     # plot the scatter points with state identities
     for i in range(K):
         plt.scatter(disttoplot_manual[i, 0], disttoplot_manual[i, 1], s=400, color=color_scheme[ordering[i],:])
         plt.text(disttoplot_manual[i, 0], disttoplot_manual[i, 1], str(ordering[i]+1), horizontalalignment='center', verticalalignment='center', fontsize=16)
+
     # plot the arrows
     for ik1 in range(K):
         for k2 in range(K):
             if sigpoints[ik1, k2]:
+                # arrow lengths have to be proportional to the distance between the states. Use Pythagoras:
                 linescale = np.sqrt(np.sum((disttoplot_manual[k2, :] - disttoplot_manual[ik1, :])**2))
                 quivlength = 0.3 * linescale
                 arrow_start = disttoplot_manual[ik1,:] + 0.1*(disttoplot_manual[k2,:] - disttoplot_manual[ik1,:])/linescale
@@ -453,6 +506,7 @@ def plot_cycle(ordering, fo_density, sigpoints, newfigure=False, color_scheme=No
                     plt.arrow(arrow_end[0], arrow_end[1],
                               arrow_start[0]-arrow_end[0], arrow_start[1]-arrow_end[1],
                               head_width=.05, head_length=.1, length_includes_head=True, color='k')
+                    
     plt.axis('off')
     plt.axis('equal')
 
