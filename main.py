6a05ed12e159f413a4d522474f9aef5c1f3e599e
Yiming Wei <weiym97@163.com>
2023-07-05 16:47:55 +0100

    first training

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 import scipy.stats as stats
 from osl_dynamics.data import Data
 from osl_dynamics.analysis import connectivity
+from osl_dynamics.models.hmm import Config, Model
 
 def swc_analysis(dataset):
     ts = dataset.time_series()
@@ -17,6 +18,32 @@ def swc_analysis(dataset):
         swc_concat[:5],
         threshold=0.95,  # only display the top 5% of connections
     )
+
+def HMM_analysis(dataset):
+    # Create a config object
+    config = Config(
+        n_states=8,
+        n_channels=80,
+        sequence_length=1000,
+        learn_means=False,
+        learn_covariances=True,
+        batch_size=16,
+        learning_rate=1e-3,
+        n_epochs=10,  # for the purposes of this tutorial we'll just train for a short period
+    )
+
+    model = Model(config)
+    model.summary()
+
+    # Initialisation
+    init_history = model.random_state_time_course_initialization(dataset, n_epochs=1, n_init=3)
+
+    # Model training
+    history = model.fit(dataset)
+
+    # Save the model
+    model.save("results/model")
+
 
 
 if __name__ == '__main__':
@@ -31,6 +58,10 @@ if __name__ == '__main__':
         assert temp.shape == (4800,15)
         np_datas.append(temp)
 
+        if len(np_datas)>10:
+            continue
+
+    print('Number of subjects: ',len(subjs))
     print('Mean of the standardised data: ',np.mean(np_datas[0],axis=0))
     print('Std of the standardised data: ', np.std(np_datas[0], axis=0))
 
@@ -39,3 +70,6 @@ if __name__ == '__main__':
 
     # Step 1: Sliding window analysis
     #swc_analysis(dataset)
+
+    # Step 2: HMM analysis
+    HMM_analysis(dataset)
