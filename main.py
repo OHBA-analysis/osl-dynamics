4e33c56eec7f8bd4bf363e173f6b3e60ae41cf68
Yiming Wei <weiym97@163.com>
2023-07-05 15:57:14 +0100

    preparing the data

--- main.py
+++ main.py
@@ -2,7 +2,22 @@ import glob
 import pathlib
 
 import numpy as np
+import scipy.stats as stats
 from osl_dynamics.data import Data
+from osl_dynamics.analysis import connectivity
+
+def swc_analysis(dataset):
+    ts = dataset.time_series()
+    swc = connectivity.sliding_window_connectivity(ts, window_length=100, step_size=50, conn_type="corr")
+    swc_concat = np.concatenate(swc)
+    swc_concat = np.abs(swc_concat)
+
+    print(swc_concat.shape)
+    connectivity.save(
+        swc_concat[:5],
+        threshold=0.95,  # only display the top 5% of connections
+    )
+
 
 if __name__ == '__main__':
     data_dir = pathlib.Path('/vols/Data/HCP/Phase2/group1200/node_timeseries/3T_HCP1200_MSMAll_d15_ts2/')
@@ -11,15 +26,16 @@ if __name__ == '__main__':
     for file in data_dir.glob('*.txt'):
         subjs.append(file.stem)
         temp = np.loadtxt(file)
+        temp = stats.zscore(temp,axis=0)
+
         assert temp.shape == (4800,15)
         np_datas.append(temp)
 
-    print('Mean of original data: ',np.mean(np_datas[0],axis=0))
+    print('Mean of the standardised data: ',np.mean(np_datas[0],axis=0))
+    print('Std of the standardised data: ', np.std(np_datas[0], axis=0))
 
 
     dataset = Data(np_datas)
-    dataset.prepare()
-    ts = dataset.time_series()
-    print('###################################')
-    print('shape of the first subject: ', ts[0].shape)
-    print('Mean of the first subject: ',np.mean(ts[0],axis=0))
+
+    # Step 1: Sliding window analysis
+    #swc_analysis(dataset)
