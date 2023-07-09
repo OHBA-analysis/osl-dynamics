6f4b0224f847acb33c7f8ed45893af6ee02e603d
Chetan Gohil <chetan.gohil@psych.ox.ac.uk>
2023-07-09 13:00:30 +0100

    Doc: updated config API documentation.

--- osl_dynamics/config_api/pipeline.py
+++ osl_dynamics/config_api/pipeline.py
@@ -1,29 +1,7 @@
 """Functions for running full pipelines via the config API.
 
-See the `toolbox examples
-<https://github.com/OHBA-analysis/osl-dynamics/tree/main/examples/toolbox_paper>`_
-for scripts that use the config API.
-
-Note, the config API can be used via the command line with::
-
-    % osl-dynamics <config-file> <output-directory>
-
-where
-
-- :code:`<config-file>` is a yaml file containing the config.
-- :code:`<output-directory>` is the output directory.
-
-Optionally, you can specify a particular GPU to use with::
-
-    % osl-dynamics <config-file> <output-directory> --restrict <restrict>
-
-where :code:`<restrict>` is an integer specifying the GPU number. E.g. if you would
-just like to use the first GPU, you can pass::
-
-    % osl-dynamics <config-file> <output-directory> --restrict 0
-
-Remember you need to activate the :code:`osld` conda environment to use the
-command line interface.
+See the documentation `here <https://osl-dynamics.readthedocs.io/en/latest/autoapi\
+/osl_dynamics/config_api/index.html>`_ for example usage.
 """
 
 import argparse
