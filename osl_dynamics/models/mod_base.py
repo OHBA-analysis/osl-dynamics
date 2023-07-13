3349671bc5278b27aad966013ccc04ff298a17f1
Chetan Gohil <chetan.gohil@psych.ox.ac.uk>
2023-07-13 20:09:18 +0100

    Refact: minor tweak to allow models without a StaticLossScalingFactorLayer.

--- osl_dynamics/models/mod_base.py
+++ osl_dynamics/models/mod_base.py
@@ -486,8 +486,9 @@ class ModelBase:
         :code:`"static_loss_scaling_factor"`, with an attribure called
         :code:`"n_batches"`.
         """
-        n_batches = dtf.get_n_batches(dataset)
-        self.model.get_layer("static_loss_scaling_factor").n_batches = n_batches
+        if "static_loss_scaling_factor" in [layer.name for layer in self.model.layers]:
+            n_batches = dtf.get_n_batches(dataset)
+            self.model.get_layer("static_loss_scaling_factor").n_batches = n_batches
 
     @contextmanager
     def set_trainable(self, layers, values):
