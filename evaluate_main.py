4a87502a04fde9e68394008c4eaa3a0b9ca346cf
weiym97 <weiym97@163.com>
2024-02-05 17:32:10 +0000

    âWIP, batch training code almost finished

--- evaluate_main.py
+++ evaluate_main.py
@@ -1,104 +1,6 @@
-import time
 import sys
 import yaml
-import json
-
 from osl_dynamics.config_api.batch import IndexParser, BatchTrain
-
-
-def model_train_wrapper(config:dict):
-    """
-    This is the new main function for model training
-    Parameters
-    ----------
-    config: dict
-        the configuration file for model training
-
-    Returns
-    -------
-    """
-    from osl_dynamics.models.hmm import Config, Model
-
-    # Currently, only HMM model works
-    if config["model"] != 'HMM':
-        raise ValueError('At present, only HMM model works!')
-    # make the directory and save the configuration files.
-    save_dir = f'{config["save_dir"]}{config["model"]}_ICA_{config["channel"]}' \
-               f'_state_{config["state"]}/{config["mode"]}/'
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-    with open(f'{save_dir}config.yaml', 'w') as file:
-        yaml.dump(config, file, default_flow_style=False)
-
-    def model_train(config):
-        import time
-        import sys
-        import yaml
-
-        # These are for the class IndexParser
-        import os
-        import random
-        import time
-        import pandas as pd
-
-        def model_train_wrapper(config: dict):
-            """
-            This is the new main function for model training
-            Parameters
-            ----------
-            config: dict
-                the configuration file for model training
-
-            Returns
-            -------
-            """
-            from osl_dynamics.models.hmm import Config, Model
-
-            # Currently, only HMM model works
-            if config["model"] != 'HMM':
-                raise ValueError('At present, only HMM model works!')
-            # make the directory and save the configuration files.
-            save_dir = f'{config["save_dir"]}{config["model"]}_ICA_{config["channel"]}' \
-                       f'_state_{config["state"]}/{config["mode"]}/'
-            if not os.path.exists(save_dir):
-                os.makedirs(save_dir)
-            with open(f'{save_dir}config.yaml', 'w') as file:
-                yaml.dump(config, file, default_flow_style=False)
-
-            def model_train(config):
-                # Record the start time before training
-                start_time = time.time()
-
-                # Initiate a Model class and print a summary
-                model = Model(config)
-                model.summary()
-
-                # Initialization
-                init_history = model.random_state_time_course_initialization(dataset, n_epochs=2, n_init=10)
-
-                # Full training
-                history = model.fit(dataset)
-                model.save(save_dir)
-
-                # loss_history = history["loss"]
-                np.save(f'{save_dir}/loss_history.npy', np.array(init_history['loss'] + history['loss']))
-
-                end_time = time.time()
-
-                # Calculate the training duration
-                training_duration = end_time - start_time
-
-                # Print or save the training duration
-                print(f"Training time: {training_duration} seconds")
-                # Save the training duration to a JSON file
-                data = {"duration": training_duration}
-
-                with open(f'{save_dir}/time.json', "w") as json_file:
-                    json.dump(data, json_file)
-
-
-
-
 
 if __name__ == '__main__':
     index = int(sys.argv[1]) - 1
@@ -107,5 +9,5 @@ if __name__ == '__main__':
         config_batch = yaml.safe_load(file)
     index_parser = IndexParser(config_batch)
     config = index_parser.parse(index)
-    batch_train = BatchTrain()
-    batch_train.model_train(config)
+    batch_train = BatchTrain(config)
+    batch_train.model_train()
--- osl_dynamics/config_api/batch.py
+++ osl_dynamics/config_api/batch.py
@@ -9,11 +9,13 @@ See ./config_train_prototype.yaml for an example batch file.
 import os
 import random
 import time
+import json
 
 import yaml
 import numpy as np
 import pandas as pd
 from .pipeline import run_pipeline_from_file
+from ..data.base import Data
 class IndexParser:
 
     """
@@ -24,55 +26,61 @@ class IndexParser:
     Given a training index, we need to find the specific batch_variable
     and combine that with other non_batch variables
     header:
-      # We assign a time stamp for each batch training
-      time: 2024-02-02T16:05:00.000Z
-      # Where to save the model training results
-      save_dir: './results_yaml_test/'
-      # where to read the data
-      data_dir: './data/node_timeseries/simulation_202402/sigma_0.1/'
-      # where to load the spatial map and spatial surface map
-      spatial_map:
-      # Add custom notes, which will also be saved
-      note: "test whether your yaml file works"
-
-    # The following variables have lists for batch training
-    batch_variable:
-      models:
-        - 'HMM'
-        - 'Dynemo'
-        - 'mDyenmo'
-        - 'SWC'
-      channels: [15,25,50,100]
-      states: [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
-      # Mode can be: train, repeat, split, cross_validation
-      mode:
-        - train
-        - repeat_1
-        - repeat_2
-        - repeat_3
-        - repeat_4
-        - repeat_5
-        - split_1
-        - split_2
-        - split_3
-        - split_4
-        - split_5
-        - cv_1
-        - cv_2
-        - cv_3
-        - cv_4
-        - cv_5
-
-    non_batch_variable:
-      learn_means: false
-      learn_covariances: true
-      learn_trans_prob: true
-      z_score_data: false
-      learning_rate: 0.01
-      split_strategy: random
-
-
-
+  # We assign a time stamp for each batch training
+  time: 2024-02-02T16:05:00.000Z
+  # Add custom notes, which will also be saved
+  note: "test whether your yaml file works"
+# where to read the data
+load_data:
+  inputs: './data/node_timeseries/simulation_202402/sigma_0.1/'
+  prepare:
+    select:
+      timepoints:
+        - 0
+        - 1200
+    standardize: {}
+# Where to save model training results
+save_dir: './results_yaml_test/'
+# where to load the spatial map and spatial surface map
+spatial_map: './data/spatial_maps/'
+non_batch_variable:
+  n_channels: 25
+  sequence_length: 600
+  learn_means: false
+  learn_covariances: true
+  learn_trans_prob: true
+  learning_rate: 0.01
+  n_epochs: 30
+  split_strategy: random
+  init_kwargs:
+    n_init: 10
+    n_epochs: 2
+# The following variables have lists for batch training
+batch_variable:
+  model:
+    - 'hmm'
+    - 'dynemo'
+    - 'mdyenmo'
+    - 'swc'
+  n_states: [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+  # Mode can be: train, repeat, split, cross_validation
+  mode:
+    - train
+    - repeat_1
+    - repeat_2
+    - repeat_3
+    - repeat_4
+    - repeat_5
+    - split_1
+    - split_2
+    - split_3
+    - split_4
+    - split_5
+    - cv_1
+    - cv_2
+    - cv_3
+    - cv_4
+    - cv_5
     """
     def __init__(self,config:dict):
 
@@ -82,6 +90,8 @@ class IndexParser:
         self.save_dir = config['save_dir']
         self.batch_variable = config['batch_variable']
         self.non_batch_variable = config['non_batch_variable']
+        self.other_keys = new_dict = {key: value for key, value in config.items()
+                                      if key not in ['batch_variable','non_batch_variable']}
 
         # Check whether the save_dir exists, make directory if not
         if not os.path.exists(config['save_dir']):
@@ -114,7 +124,7 @@ class IndexParser:
 
         # concatenate three parts of the dictionary
         new_config = {}
-        new_config['save_dir'] = self.save_dir
+        new_config.update(self.other_keys)
         new_config.update(bv)
         new_config.update(self.non_batch_variable)
 
@@ -139,89 +149,117 @@ class BatchTrain:
     Convert a batch training configuration file to another config
     for training pipeline
     """
+    mode_key_default = 'mode'
+
     train_keys_default = ['n_channels',
                           'n_states',
-                          'sequence_length',
                           'learn_means',
                           'learn_covariances',
                           'learn_trans_prob',
+                          'sequence_length',
+                          'batch_size',
                           'learning_rate',
-                          'n_epochs'
+                          'n_epochs',
                           ]
-    def __init__(self,train_keys=None):
+    def __init__(self,config:dict,train_keys=None):
         self.train_keys = self.train_keys_default if train_keys is None else train_keys
 
-    def model_train(self, config:dict):
+        # Validate the configuration file
+        if 'load_data' not in config:
+            raise ValueError('No data directory specified!')
+        # The default mode of 'mode' is train
+        if 'mode' not in config:
+            config['mode'] = 'train'
+        if 'init_kwargs' not in config:
+            config['init_kwargs'] = {}
+        # Check whether save directory is specified
+        if 'save_dir' not in config:
+            raise ValueError('Saving directory not specified!')
+        else:
+            config['save_dir'] = f'{config["save_dir"]}{config["model"]}' \
+                                 f'_ICA_{config["n_channels"]}_state_{config["n_states"]}/{config["mode"]}/'
+        if not os.path.exists(config['save_dir']):
+            os.makedirs(config['save_dir'])
+        with open(f'{config["save_dir"]}batch_config.yaml','w') as file:
+            yaml.safe_dump(config,file, default_flow_style=False)
+        self.config = config
+
+    def model_train(self,cv_ratio=0.8):
         '''
         Batch model train method
-        Parameters
-        ----------
-        config: dict
-            the original configuration from the batch.
-
+        cv_ration: float,optional
+           the proportion of sessions to use as the training data
         Returns
         -------
-
         '''
-        # data_dir need to be specified in the config
-        if 'inputs' not in config:
-            raise ValueError('No data directory specified!')
-        # if prepare is not in the config, add 'prepare':{} to config
-        if 'prepare' not in config:
-            config['prepare'] = {}
-        train_config = {'load_data':self.copy_key_value(['inputs','prepare'],config)}
-        config_kwargs = self.copy_key_value(self.train_keys,config)
-        init_kwargs = self.copy_key_value(["init_kwargs"],config)
+        prepare_config = {}
+        prepare_config['load_data'] = self.config['load_data']
 
-        train_config[f'train_{config["model"]}'] = {'config_kwargs':config_kwargs,
-                                                    'init_kwargs':init_kwargs}
+        prepare_config[f'train_{self.config["model"]}'] = {
+            'config_kwargs':
+                {key: self.config[key] for key in self.train_keys if key in self.config},
+            'init_kwargs':
+                self.config['init_kwargs']
+        }
 
-        save_dir = f'{config["save_dir"]}{config["model"]}_ICA' \
-                   f'_{config["n_channels"]}_state_{config["n_states"]}/'
-        if not os.path.exists(save_dir):
-            os.makedirs(save_dir)
+        if "split" in self.config["mode"]:
+            # We need to know how many sessions in advance
+            indice_1, indice_2 = self.select_indice()
 
-        with open(f'{save_dir}general_config.yaml', 'w') as file:
-            yaml.dump(config, file)
+            # Save the selected and remaining indices to JSON files
+            with open(f'{self.config["save_dir"]}indices_1.json', 'w') as json_file:
+                json.dump(indice_1, json_file)
+            with open(f'{self.config["save_dir"]}indices_2.json', 'w') as json_file:
+                json.dump(indice_2, json_file)
+
+            for i in range(0,2):
+                temp_save_dir = f'{self.config["save_dir"]}half_{i+1}/'
+                if not os.path.exists(temp_save_dir):
+                    os.makedirs(temp_save_dir)
+                prepare_config['keep_list'] = f'{self.config["save_dir"]}indices_{i+1}.json'
+                with open(f'{temp_save_dir}prepared_config.yaml', 'w') as file:
+                    yaml.safe_dump(prepare_config, file, default_flow_style=False)
+                #run_pipeline_from_file(f'{self.config["save_dir"]}prepared_config.yaml',
+                #                      temp_save_dir)
 
 
+        elif "cv" in self.config["mode"]:
+            indice_1, indice_2 = self.select_indice(ratio=cv_ratio)
 
-        save_dir = f'{save_dir}{config["mode"]}/'
-        if not os.path.exists(save_dir):
-            os.makedirs(save_dir)
-        with open(f'{save_dir}train_config.yaml', 'w') as file:
-            yaml.dump(train_config, file)
-        if "split" in config["mode"]:
-            pass
-        elif "cv" in config["mode"]:
-            pass
+            # Save the selected and remaining indices to JSON files
+            with open(f'{self.config["save_dir"]}indices_train.json', 'w') as json_file:
+                json.dump(indice_1, json_file)
+            with open(f'{self.config["save_dir"]}indices_validate.json', 'w') as json_file:
+                json.dump(indice_2, json_file)
+
+            prepare_config['keep_list'] = f'{self.config["save_dir"]}indices_train.json'
+            with open(f'{self.config["save_dir"]}prepared_config.yaml', 'w') as file:
+                yaml.safe_dump(prepare_config, file, default_flow_style=False)
+            #run_pipeline_from_file(f'{self.config["save_dir"]}prepared_config.yaml',
+            #                       self.config['save_dir'])
+
+
         else:
-            run_pipeline_from_file(f'{save_dir}train_config.yaml',save_dir)
+            with open(f'{self.config["save_dir"]}prepared_config.yaml', 'w') as file:
+                yaml.safe_dump(prepare_config, file, default_flow_style=False)
+            #run_pipeline_from_file(f'{self.config["save_dir"]}prepared_config.yaml',
+            #                       self.config["save_dir"])
 
 
+    def select_indice(self,ratio=0.5):
+        if "n_sessions" not in self.config:
+            data = Data(self.config["load_data"]["inputs"])
+            n_sessions = len(data.arrays)
+        else:
+            n_sessions = self.config["n_sessions"]
 
+        all_indices = list(range(n_sessions))
+        # Calculate the number of indices to select (half of the total)
+        n_selected_sessions = int(n_sessions * ratio)
 
+        # Randomly select indices without replacement
+        selected_indices = random.sample(all_indices, n_selected_sessions)
 
-
-
-    def copy_key_value(self,keys:list,source:dict,dest:dict=None):
-        """
-        Copy the (key,value) pair from source to dest. dest can be none at the beginning.
-        Parameters
-        ----------
-        source: dict
-            source dictionary
-        dest: dict
-            destination dictionary
-        keys: list
-            keys to copy
-        Returns
-        -------
-        dest: dict
-            updated configuration
-        """
-        if dest is None:
-            dest = {}
-        for key in keys:
-            dest[key] = source[key]
-        return dest
+        # Calculate the remaining indices
+        remaining_indices = list(set(all_indices) - set(selected_indices))
+        return selected_indices,remaining_indices
