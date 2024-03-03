0498a48d3313d267ac49219366c288707610e787
weiym97 <weiym97@163.com>
2024-03-03 09:08:09 +0000

    Unit test for osl_dynamics.inference.metrics & modes

--- tests/test_inference/test_metrics.py
+++ tests/test_inference/test_metrics.py
@@ -11,3 +11,45 @@ def test_alpha_correlation():
                             np.array([[1., -0.5], [0.5, -1]]))
     npt.assert_almost_equal(alpha_correlation(alpha_1, alpha_2, return_diagonal=True),
                             np.array([1., -1.]))
+
+def test_confusion_matrix():
+    from osl_dynamics.inference.metrics import confusion_matrix
+
+    # Define two example state time courses
+    state_time_course_1 = np.array([0, 1, 1, 0, 2])  # Example states
+    state_time_course_2 = np.array([0, 1, 2, 2, 2])  # Example states
+
+    # Expected confusion matrix
+    expected_cm = np.array([[1, 0, 1],
+                            [0, 1, 1],
+                            [0, 0, 1]])
+
+    # Calculate the confusion matrix
+    cm = confusion_matrix(state_time_course_1, state_time_course_2)
+
+    # Check if the calculated confusion matrix matches the expected one
+    npt.assert_equal(cm, expected_cm)
+
+def test_dice_coefficient():
+    from osl_dynamics.inference.metrics import dice_coefficient
+    # Test case 1: One-dimensional input
+    sequence_1 = np.array([0, 1, 1, 0, 2])
+    sequence_2 = np.array([0, 1, 2, 2, 2])
+    expected_dice = 0.6
+    npt.assert_equal(dice_coefficient(sequence_1,sequence_2),expected_dice)
+
+    # Test case 2:  two-dimensional input
+    sequence_1 = np.array([
+        [0.1, 0.2, 0.3],
+        [1.0, 0.5, 0.0],
+        [0.0, 1.0, 0.0],
+        [0.4,0.5,0.6]
+    ])
+    sequence_2 = np.array([
+        [0.1,0.3,0.4],
+        [0.5, 1.0, 0.0],
+        [0.0, 1.0, 0.0],
+        [0.6,0.5,0.4]
+    ])
+    expected_dice = 0.5
+    npt.assert_equal(dice_coefficient(sequence_1, sequence_2), expected_dice)
---
+++ tests/test_inference/test_modes.py
@@ -0,0 +1,55 @@
+import numpy as np
+import numpy.testing as npt
+
+def test_argmax_time_courses():
+    from osl_dynamics.inference.modes import argmax_time_courses
+
+    # Test case 1: When alpha is a list, concatenate is false
+    alpha_list = [np.array([[0.1, 0.9], [0.8, 0.2]]), np.array([[0.2, 0.8], [0.6, 0.4]])]
+    expected_result = np.array([[[0, 1], [1, 0]], [[0, 1], [1, 0]]])
+    result = argmax_time_courses(alpha_list, concatenate=False)
+    npt.assert_array_equal(result, expected_result)
+
+    # Test case 2: When alpha is a list, concatenate is true
+    expected_result = np.array([[0, 1], [1, 0], [0, 1], [1, 0]])
+    result = argmax_time_courses(alpha_list, concatenate=True)
+    npt.assert_array_equal(result, expected_result)
+
+    # Test case 3: When alpha is a 2D numpy array
+    alpha_2d = np.array([[0.1, 0.9], [0.8, 0.2]])
+    expected_result = np.array([[0, 1], [1, 0]])
+    result = argmax_time_courses(alpha_2d)
+    npt.assert_array_equal(result, expected_result)
+
+    # Test case 4: When alpha is a 3D numpy array
+    alpha_3d = np.array([[[0.1, 0.9], [0.8, 0.2]], [[0.2, 0.8], [0.6, 0.4]]])
+    expected_result = np.array([[[0, 1], [1, 0]], [[0, 1], [1, 0]]])
+    result = argmax_time_courses(alpha_3d)
+    npt.assert_array_equal(result, expected_result)
+
+    
+def test_reduce_state_time_course():
+    from osl_dynamics.inference.modes import reduce_state_time_course
+
+    state_time_course = np.array([
+        [1, 0, 1],
+        [0, 1, 0],
+        [0, 0, 0],
+        [1, 1, 1]
+    ]).T
+    expected_result = np.array([
+        [1, 0, 1],
+        [0, 1, 0],
+        [1, 1, 1]
+    ]).T
+    reduced_state_time_course = reduce_state_time_course(state_time_course)
+    npt.assert_equal(reduced_state_time_course, expected_result)
+
+def test_correlate_modes():
+    from osl_dynamics.inference.modes import correlate_modes
+
+    alpha_1 = np.array([[1, 0, -1], [1, -1, 0]]).T
+    alpha_2 = np.array([[1, 0, -1], [-1, 1, 0]]).T
+
+    npt.assert_almost_equal(correlate_modes(alpha_1, alpha_2),
+                            np.array([[1., -0.5], [0.5, -1]]))
