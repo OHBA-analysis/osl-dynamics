6b2d1ca8ea7a87383ddf3e64bd7b716195e64b43
Rukuang Huang <rukuang.huang@jesus.ox.ac.uk>
2024-04-13 21:31:48 +0100

    Fix: Fixed HIVE model.

--- osl_dynamics/models/hive.py
+++ osl_dynamics/models/hive.py
@@ -30,6 +30,7 @@ from osl_dynamics.inference.layers import (
     TFGatherLayer,
     AddLayer,
     ConstrainedEmbeddingLayer,
+    TFConstantLayer,
 )
 from osl_dynamics.models import obs_mod
 from osl_dynamics.models.mod_base import BaseModelConfig
@@ -333,6 +334,14 @@ class Model(MarkovStateInferenceModelBase):
             kl_loss_layer = self.model.get_layer("kl_loss")
             kl_loss_layer.annealing_factor.assign(0.0)
 
+            if "means_dev_mag" in self.model.layers:
+                means_dev_mag_layer = self.model.get_layer("means_dev_mag")
+                means_dev_mag_layer.annealing_factor.assign(0.0)
+
+            if "covs_dev_mag" in self.model.layers:
+                covs_dev_mag_layer = self.model.get_layer("covs_dev_mag")
+                covs_dev_mag_layer.annealing_factor.assign(0.0)
+
     def get_group_means(self):
         """Get the group level state means.
 
@@ -617,52 +626,44 @@ def _model_structure(config):
     )
     static_loss_scaling_factor = static_loss_scaling_factor_layer(data)
 
-    # Session labels input
-    session_labels = []
+    # Session labels input (This is for calculating the LL loss)
+    session_id = layers.Input(
+        shape=(config.sequence_length,),
+        dtype=tf.int32,
+        name="session_id",
+    )
+    session_label_layers = dict()
+    session_labels = dict()
+    label_embeddings_layers = dict()
+    label_embeddings = []
     for session_label in config.session_labels:
-        session_labels.append(
-            layers.Input(
-                shape=(config.sequence_length,),
-                dtype=(
-                    tf.int32
-                    if session_label.label_type == "categorical"
-                    else tf.float32
-                ),
-                name=session_label.name,
+        session_label_layers[session_label.name] = TFConstantLayer(
+            values=session_label.values,
+            name=f"{session_label.name}_constant",
+        )
+        session_labels[session_label.name] = session_label_layers[session_label.name](
+            data
+        )
+        label_embeddings_layers[session_label.name] = (
+            layers.Embedding(
+                session_label.n_classes,
+                config.embeddings_dim,
+                name=f"{session_label.name}_embeddings",
+            )
+            if session_label.label_type == "categorical"
+            else layers.Dense(
+                config.embeddings_dim, name=f"{session_label.name}_embeddings"
             )
         )
-
-    session_label_embeddings_layers = []
-    for session_label in config.session_labels:
-        if session_label.label_type == "categorical":
-            session_label_embeddings_layers.append(
-                ConstrainedEmbeddingLayer(
-                    session_label.n_classes,
-                    config.embeddings_dim,
-                    name=f"{session_label.name}_embeddings",
-                )
+        label_embeddings.append(
+            label_embeddings_layers[session_label.name](
+                session_labels[session_label.name]
             )
-        else:
-            session_label_embeddings_layers.append(
-                layers.Dense(
-                    config.embeddings_dim, name=f"{session_label.name}_embeddings"
-                ),
-            )
-
-    tf_gather_0_layer = TFGatherLayer(axis=0)
-    tf_gather_1_layer = TFGatherLayer(axis=1)
-    session_label_embeddings = []
-    # session_label_embeddings[0].shape = (None, embeddings_dim)
-    for session_label, session_label_embeddings_layer in zip(
-        session_labels, session_label_embeddings_layers
-    ):
-        session_label_embeddings.append(
-            session_label_embeddings_layer(tf_gather_1_layer([session_label, 0]))
         )
 
     embeddings_layer = AddLayer(name="embeddings")
-    embeddings = embeddings_layer(session_label_embeddings)
-    # embeddings.shape = (None, embeddings_dim)
+    embeddings = embeddings_layer(label_embeddings)
+    # embeddings.shape = (n_sessions, embeddings_dim)
 
     # Group level observation model parameters
     group_means_layer = VectorsLayer(
@@ -754,7 +755,7 @@ def _model_structure(config):
         means_spatial_embeddings = means_spatial_embeddings_layer(group_mu)
         means_concat_embeddings = means_concat_embeddings_layer(
             [embeddings, means_spatial_embeddings]
-        )  # shape = (None, n_states, embeddings_dim + spatial_embeddings_dim)
+        )  # shape = (n_sessions, n_states, embeddings_dim + spatial_embeddings_dim)
 
         # Get the mean deviation maps (no global magnitude information)
         means_dev_decoder = means_dev_decoder_layer(
@@ -762,26 +763,28 @@ def _model_structure(config):
             static_loss_scaling_factor=static_loss_scaling_factor,
         )
         means_dev_map = means_dev_map_layer(means_dev_decoder)
-        norm_means_dev_map = norm_means_dev_map_layer(means_dev_map)
-        # norm_means_dev_map.shape = (None, n_states, n_channels)
+        norm_means_dev_map = tf.gather(
+            norm_means_dev_map_layer(means_dev_map),
+            session_id[:, 0],
+            axis=0,
+        )
+        # shape = (None, n_states, n_channels)
 
         # Get the deviation magnitudes (scale deviation maps globally)
         means_dev_mag_inf_alpha_input = means_dev_mag_inf_alpha_input_layer(data)
-        means_dev_mag_inf_alpha_input = tf_gather_0_layer(
-            [means_dev_mag_inf_alpha_input, tf_gather_1_layer([session_labels[0], 0])]
-        )
         means_dev_mag_inf_alpha = means_dev_mag_inf_alpha_layer(
             means_dev_mag_inf_alpha_input
         )
         means_dev_mag_inf_beta_input = means_dev_mag_inf_beta_input_layer(data)
-        means_dev_mag_inf_beta_input = tf_gather_0_layer(
-            [means_dev_mag_inf_beta_input, tf_gather_1_layer([session_labels[0], 0])]
-        )
         means_dev_mag_inf_beta = means_dev_mag_inf_beta_layer(
             means_dev_mag_inf_beta_input
         )
         means_dev_mag = means_dev_mag_layer(
-            [means_dev_mag_inf_alpha, means_dev_mag_inf_beta]
+            [
+                means_dev_mag_inf_alpha,
+                means_dev_mag_inf_beta,
+                session_id,
+            ]
         )
         means_dev = means_dev_layer([means_dev_mag, norm_means_dev_map])
     else:
@@ -868,23 +871,26 @@ def _model_structure(config):
             static_loss_scaling_factor=static_loss_scaling_factor,
         )
         covs_dev_map = covs_dev_map_layer(covs_dev_decoder)
-        norm_covs_dev_map = norm_covs_dev_map_layer(covs_dev_map)
+        norm_covs_dev_map = tf.gather(
+            norm_covs_dev_map_layer(covs_dev_map),
+            session_id[:, 0],
+            axis=0,
+        )
 
         # Get the deviation magnitudes (scale deviation maps globally)
         covs_dev_mag_inf_alpha_input = covs_dev_mag_inf_alpha_input_layer(data)
-        covs_dev_mag_inf_alpha_input = tf_gather_0_layer(
-            [covs_dev_mag_inf_alpha_input, tf_gather_1_layer([session_labels[0], 0])]
-        )
         covs_dev_mag_inf_alpha = covs_dev_mag_inf_alpha_layer(
             covs_dev_mag_inf_alpha_input
         )
         covs_dev_mag_inf_beta_input = covs_dev_mag_inf_beta_input_layer(data)
-        covs_dev_mag_inf_beta_input = tf_gather_0_layer(
-            [covs_dev_mag_inf_beta_input, tf_gather_1_layer([session_labels[0], 0])]
-        )
         covs_dev_mag_inf_beta = covs_dev_mag_inf_beta_layer(covs_dev_mag_inf_beta_input)
+
         covs_dev_mag = covs_dev_mag_layer(
-            [covs_dev_mag_inf_alpha, covs_dev_mag_inf_beta]
+            [
+                covs_dev_mag_inf_alpha,
+                covs_dev_mag_inf_beta,
+                session_id,
+            ]
         )
         # covs_dev_mag.shape = (None, n_states, 1)
         covs_dev = covs_dev_layer([covs_dev_mag, norm_covs_dev_map])
@@ -974,6 +980,7 @@ def _model_structure(config):
                 means_dev_mag_inf_beta,
                 means_dev_mag_mod_beta,
             ],
+            static_loss_scaling_factor=static_loss_scaling_factor,
         )
     else:
         means_dev_mag_kl_loss_layer = ZeroLayer(
@@ -1004,6 +1011,7 @@ def _model_structure(config):
                 covs_dev_mag_inf_beta,
                 covs_dev_mag_mod_beta,
             ],
+            static_loss_scaling_factor=static_loss_scaling_factor,
         )
     else:
         covs_dev_mag_kl_loss_layer = ZeroLayer((), name="covs_dev_mag_kl_loss")
@@ -1017,7 +1025,7 @@ def _model_structure(config):
     kl_loss = kl_loss_layer([means_dev_mag_kl_loss, covs_dev_mag_kl_loss])
 
     return tf.keras.Model(
-        inputs=[data] + session_labels,
+        inputs=[data, session_id],
         outputs=[ll_loss, kl_loss, gamma, xi],
         name="HIVE",
     )
