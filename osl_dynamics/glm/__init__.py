8cdb487dc8fa8753701c3898c668cb8d6d545e90
Rukuang Huang <rukuang.huang@jesus.ox.ac.uk>
2024-10-04 11:16:54 +0100

    Release: Version 1.4.5.

--- osl_dynamics/glm/__init__.py
+++ osl_dynamics/glm/__init__.py
@@ -1,4 +1,4 @@
 """Tools for fitting General Linear Models (GLMs) and performing permutation tests."""
 
-from .base import DesignConfig, GLM
-from .permutation import MaxStatPermutation
+from osl_dynamics.glm.base import DesignConfig, GLM
+from osl_dynamics.glm.permutation import MaxStatPermutation
