4d69e0ec60be7ea511d4e45dc36ac4fc4b47127d
weiym97 <weiym97@163.com>
2025-03-10 11:53:28 +0000

    Add infer spatial in BiCrossValidation

--- osl_dynamics/evaluate/cross_validation.py
+++ osl_dynamics/evaluate/cross_validation.py
@@ -227,6 +227,32 @@ class BiCrossValidation:
             yaml.safe_dump(config, file, default_flow_style=False)
         return train_model(**config)
 
+    def infer_spatial(self, row, column, temporal_params, save_dir=None,):
+        from osl_dynamics.config_api.wrappers import infer_spatial
+        # Specify the save directory
+        if save_dir is None:
+            save_dir = os.path.join(self.save_dir, 'infer_spatial/')
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+
+        # Prepare the config
+        config = {}
+        config['model_type'] = self.model
+        config['data'] = self._prepare_load_data_config(row, column, save_dir)
+        config['output_dir'] = save_dir
+
+        # Add the spatial parameters
+        config['temporal_params'] = temporal_params
+
+        # Add keys only if they exist in self.model_kwargs
+        for key in ["config_kwargs", "init_kwargs", "fit_kwargs"]:
+            if key in self.model_kwargs:
+                config[key] = self.model_kwargs[key]
+
+        with open(f'{save_dir}/config.yaml', 'w') as file:
+            yaml.safe_dump(config, file, default_flow_style=False)
+        return infer_spatial(**config)
+
     def infer_temporal(self,row,column,spatial_params,save_dir=None):
         from osl_dynamics.config_api.wrappers import infer_temporal
         # Specify the save directory
@@ -252,6 +278,7 @@ class BiCrossValidation:
         with open(f'{save_dir}/config.yaml', 'w') as file:
             yaml.safe_dump(config, file, default_flow_style=False)
         return infer_temporal(**config)
+
 
     def validate(self):
         if self.bcv_variant == 'fu_perry':
