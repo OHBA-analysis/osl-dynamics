Bug calculating the free energy of a loaded model (when the KL annealing factor is initially 0).

osl_dynamics/inference/layers.py
@@ -1358,21 +1358,20 @@ class KLLossLayer(layers.Layer):
     def __init__(self, do_annealing, **kwargs):
         super().__init__(**kwargs)
         if do_annealing:
-            self.annealing_factor = tf.Variable(
-                0.0, trainable=False, name="kl_anneal_factor"
-            )
+            self.annealing_factor = tf.Variable(0.0, trainable=False, name="kl_factor")
         else:
-            self.annealing_factor = tf.Variable(
-                1.0, trainable=False, name="kl_anneal_factor"
-            )
+            self.annealing_factor = tf.Variable(1.0, trainable=False, name="kl_factor")
 
-    def call(self, inputs, **kwargs):
+    def call(self, inputs, training=False, **kwargs):
+        kl_loss = inputs
+
         if isinstance(inputs, list):
             # Sum KL divergences
-            inputs = tf.add_n(inputs)
+            kl_loss = tf.add_n(kl_loss)
 
-        # KL annealing
-        kl_loss = tf.multiply(inputs, self.annealing_factor)
+        if training:
+            # KL annealing
+            kl_loss = tf.multiply(kl_loss, self.annealing_factor)
 
         # Add to loss
         self.add_loss(kl_loss)