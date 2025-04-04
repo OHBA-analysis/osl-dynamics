228206b0de741338d01f3bb29a789653f88a0c22
weiym97 <weiym97@163.com>
2025-04-04 16:31:19 +0100

    DyNeMo new simulation

--- examples/simulation/dynemo_soft-mixing.py
+++ examples/simulation/dynemo_soft-mixing.py
@@ -73,7 +73,7 @@ plotting.plot_separate_time_series(
 # Build model
 model = Model(config)
 model.summary()
-#model.set_covariances(sim.covariances)
+model.set_covariances(sim.covariances)
 
 print("Training model")
 #init_kwargs = {"n_init": 10, "n_epochs": 2, "take": 1}
