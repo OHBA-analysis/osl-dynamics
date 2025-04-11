9b4d6d17446d10b10b6d1eb71ba65de3d95c63c2
cgohil8 <chetan.gohil@psych.ox.ac.uk>
2025-04-12 09:33:45 +1000

    Updated header.

--- osl_dynamics/files/__init__.py
+++ osl_dynamics/files/__init__.py
@@ -1,37 +1,6 @@
 """Files included within osl-dynamics.
 
------
-
-Available parcellations:
-
-- fMRI_parcellation_ds8mm.nii.gz
-- fmri_d100_parcellation_with_PCC_reduced_2mm_ss5mm_ds8mm.nii.gz (the 'Giles parcellation')
-- fmri_d100_parcellation_with_PCC_tighterMay15_v2_8mm.nii.gz
-- fmri_d100_parcellation_with_PCC_tighterMay15_v2_6mm_exclusive.nii.gz
-- fmri_d100_parcellation_with_3PCC_ips_reduced_2mm_ss5mm_ds8mm.nii.gz
-- fmri_d100_parcellation_with_3PCC_ips_reduced_2mm_ss5mm_ds8mm_adj.nii.gz
-- aal_cortical_merged_8mm_stacked.nii.gz
-- Glasser50_space-MNI152NLin6_res-8x8x8.nii.gz
-- Glasser52_binary_space-MNI152NLin6_res-8x8x8.nii.gz (the 'Glasser parcellation')
-- giles_39_binary.nii.gz
-
-If you use the Glasser52_binary_space-MNI152NLin6_res-8x8x8.nii.gz parcellation, please cite:
-
-    Kohl, O., Woolrich, M., Nobre, A. C., & Quinn, A. (2023). Glasser52: A parcellation for MEG-Analysis [Data set].
-    Zenodo. https://zenodo.org/records/11099418
-
-See the link for further info regarding the parcel name/locations.
-
------
-
-Available masks:
-
-- MNI152_T1_1mm_brain.nii.gz
-- MNI152_T1_2mm_brain.nii.gz
-- MNI152_T1_8mm_brain.nii.gz
-- ft_8mm_brain_mask.nii.gz
-
------
+The available files are listed in each submodule.
 """
 
 from osl_dynamics.files import mask, parcellation, scanner, scene
