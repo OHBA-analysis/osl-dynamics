aca4131015766d8e23d7e4d36f5730590ab10768
scho97 <sungjun.cho@ndcn.ox.ac.uk>
2025-04-16 00:33:17 +0100

    add: example simulation scripts for DyNeStE

---
+++ examples/simulation/dyneste_hmm-mvn.py
@@ -0,0 +1,152 @@
+"""Example script for running DyNeStE on simulated HMM-MVN data.
+
+This script should take less than a couple minutes to run and
+achieve a dice coefficient of ~0.99.
+"""
+
+print("Importing packages")
+
+import os
+import pickle
+import numpy as np
+
+from osl_dynamics.simulation import HMM_MVN
+from osl_dynamics.data import Data
+from osl_dynamics.models.dyneste import Config, Model
+from osl_dynamics.inference import modes, metrics
+from osl_dynamics.inference.callbacks import GumbelSoftmaxAnnealingCallback
+
+# Create directory for results
+results_dir = "results"
+os.makedirs(results_dir, exist_ok=True)
+
+#%% Simulate data
+
+print("Simulating data")
+sim = HMM_MVN(
+    n_samples=25600,
+    n_states=5,
+    n_channels=20,
+    trans_prob="sequence",
+    stay_prob=0.9,
+    means="zero",
+    covariances="random",
+)
+
+# Create Data object for training
+data = Data(sim.time_series)
+
+# Prepare data
+data.standardize()
+
+#%% Build model
+
+config = Config(
+    n_states=5,
+    n_channels=20,
+    sequence_length=200,
+    inference_n_units=128,
+    inference_normalization="layer",
+    model_n_units=128,
+    model_normalization="layer",
+    learn_means=False,
+    learn_covariances=True,
+    initial_gs_temperature=1.0,
+    do_kl_annealing=True,
+    kl_annealing_curve="tanh",
+    kl_annealing_sharpness=10,
+    n_kl_annealing_epochs=50,
+    batch_size=16,
+    learning_rate=5e-3,
+    lr_decay=0.01,
+    n_epochs=120,
+)
+
+model = Model(config)
+model.summary()
+
+# Add regularization for the observation model
+model.set_regularizers(data)
+
+#%% Train model
+
+# Initialization
+init_history = model.random_subset_initialization(data, n_init=5, n_epochs=1, take=1)
+
+# Full training
+history = model.fit(
+    data,
+    callbacks=[
+        GumbelSoftmaxAnnealingCallback(
+            curve="exp",
+            layer_name="states",
+            n_epochs=config.n_epochs,
+            start_temperature=config.initial_gs_temperature,
+            end_temperature=0.06,
+            slope=0.04,
+        ),
+    ],
+)
+
+# Save model
+model_dir = f"{results_dir}/model"
+model.save(model_dir)
+
+# Calculate the free energy
+free_energy = model.free_energy(data)
+print("Free energy", free_energy)
+history["free_energy"] = free_energy
+
+# Save training history and free energy
+pickle.dump(init_history, open(f"{model_dir}/init_history.pkl", "wb"))
+pickle.dump(history, open(f"{model_dir}/history.pkl", "wb"))
+
+#%% Get inferred parameters
+
+# Inferred state probabilities
+alp = model.get_alpha(data)
+
+# Observation model parameters
+means, covs = model.get_means_covariances()
+
+# Save
+inf_params_dir = f"{results_dir}/inf_params"
+os.makedirs(inf_params_dir, exist_ok=True)
+
+pickle.dump(alp, open(f"{inf_params_dir}/alp.pkl", "wb"))
+np.save(f"{inf_params_dir}/means.npy", means)
+np.save(f"{inf_params_dir}/covs.npy", covs)
+
+#%% Calculate summary statistics
+
+# State time courses
+stc = modes.argmax_time_courses(alp)
+
+# Calculate summary statistics
+fo = modes.fractional_occupancies(stc)
+lt = modes.mean_lifetimes(stc)
+intv = modes.mean_intervals(stc)
+sr = modes.switching_rates(stc)
+
+# Save
+summary_stats_dir = f"{results_dir}/summary_stats"
+os.makedirs(summary_stats_dir, exist_ok=True)
+
+np.save(f"{summary_stats_dir}/fo.npy", fo)
+np.save(f"{summary_stats_dir}/lt.npy", lt)
+np.save(f"{summary_stats_dir}/intv.npy", intv)
+np.save(f"{summary_stats_dir}/sr.npy", sr)
+
+#%% Compare inferred parameters to ground truth simulation
+
+# Re-order simulated state time course to match inferred
+inf_stc, sim_stc = modes.match_modes(stc, sim.state_time_course)
+
+# Calculate dice coefficient
+dice = metrics.dice_coefficient(inf_stc, sim_stc)
+
+print("Dice coefficient:", dice)
+
+#%% Delete temporary directory
+
+data.delete_dir()
---
+++ examples/simulation/dyneste_long-range-dep.py
@@ -0,0 +1,145 @@
+"""Example script for demonstrating DyNeStE's ability to learn long-range dependependcies.
+
+- An HSMM is simulated.
+- The output of this script will vary slightly due to random sampling.
+"""
+
+print("Setting up")
+import os
+from osl_dynamics import data, simulation
+from osl_dynamics.inference import tf_ops, modes, metrics
+from osl_dynamics.inference.callbacks import GumbelSoftmaxAnnealingCallback
+from osl_dynamics.models.dyneste import Config, Model
+from osl_dynamics.utils import plotting
+
+# Make directory for results and plots
+os.makedirs("results", exist_ok=True)
+os.makedirs("figures", exist_ok=True)
+
+# GPU settings
+tf_ops.gpu_growth()
+
+# Settings
+config = Config(
+    n_states=3,
+    n_channels=11,
+    sequence_length=200,
+    inference_n_units=128,
+    inference_normalization="layer",
+    model_n_units=128,
+    model_normalization="layer",
+    learn_means=False,
+    learn_covariances=True,
+    initial_gs_temperature=1.0,
+    do_kl_annealing=True,
+    kl_annealing_curve="tanh",
+    kl_annealing_sharpness=10,
+    n_kl_annealing_epochs=50,
+    batch_size=16,
+    learning_rate=5e-3,
+    lr_decay=0.01,
+    n_epochs=120,
+)
+
+# Simulate data
+print("Simulating data")
+sim = simulation.HSMM_MVN(
+    n_samples=25600,
+    n_channels=config.n_channels,
+    n_states=config.n_states,
+    means="zero",
+    covariances="random",
+    observation_error=0.0,
+    gamma_shape=10,
+    gamma_scale=5,
+)
+sim.standardize()
+training_data = data.Data(sim.time_series)
+
+# Plot the transition probability matrix for state switching in the HSMM
+plotting.plot_matrices(
+    sim.off_diagonal_trans_prob, filename="figures/sim_trans_prob.png"
+)
+
+# Create tensorflow datasets for training and model evaluation
+training_dataset = training_data.dataset(
+    config.sequence_length, config.batch_size, shuffle=True
+)
+prediction_dataset = training_data.dataset(
+    config.sequence_length, config.batch_size, shuffle=False
+)
+
+# Build model
+model = Model(config)
+model.summary()
+
+print("Training model")
+history = model.fit(
+    training_dataset,
+    callbacks=[
+        GumbelSoftmaxAnnealingCallback(
+            curve="exp",
+            layer_name="states",
+            n_epochs=config.n_epochs,
+            start_temperature=config.initial_gs_temperature,
+            end_temperature=0.06,
+            slope=0.04,
+        ),
+    ],
+)
+
+# Save model
+model_dir = "results/model"
+model.save(model_dir)
+
+# Free energy = Negative Log Likelihood + KL Divergence
+free_energy = model.free_energy(prediction_dataset)
+print(f"Free energy: {free_energy}")
+
+# Inferred state probabilities
+inf_alp = model.get_alpha(prediction_dataset)
+
+# State time courses
+sim_stc = sim.mode_time_course
+inf_stc = modes.argmax_time_courses(inf_alp)
+
+# Calculate the dice coefficient between state time courses
+orders = modes.match_modes(sim_stc, inf_stc, return_order=True)
+inf_stc = inf_stc[:, orders[1]]
+print("Dice coefficient:", metrics.dice_coefficient(sim_stc, inf_stc))
+
+plotting.plot_alpha(
+    sim_stc,
+    inf_stc,
+    y_labels=["Ground Truth", "DyNeStE"],
+    filename="figures/compare.png",
+)
+
+plotting.plot_state_lifetimes(
+    sim_stc, x_label="Lifetime", y_label="Occurrence", filename="figures/sim_lt.png"
+)
+plotting.plot_state_lifetimes(
+    inf_stc, x_label="Lifetime", y_label="Occurrence", filename="figures/inf_lt.png"
+)
+
+# Ground truth vs inferred covariances
+sim_cov = sim.covariances
+inf_cov = model.get_covariances()[orders[1]]
+
+plotting.plot_matrices(sim_cov, filename="figures/sim_cov.png")
+plotting.plot_matrices(inf_cov, filename="figures/inf_cov.png")
+
+# Sample from model RNN
+sam_alp = model.sample_alpha(25600)
+sam_stc = modes.argmax_time_courses(sam_alp)
+
+plotting.plot_state_lifetimes(
+    sam_stc,
+    x_label="Lifetime",
+    x_range=[0, 150],
+    y_label="Occurrence",
+    filename="figures/sam_lt.png",
+)
+
+# Delete temporary directory
+training_data.delete_dir()
