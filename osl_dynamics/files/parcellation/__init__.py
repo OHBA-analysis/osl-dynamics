eed94c17a52cdeeef7427a264b706176314df671
Chetan Gohil <chetan.gohil@psych.ox.ac.uk>
2026-01-26 20:54:12 +0000

    Removed fmri_d100_parcellation_with_PCC_tighterMay15_v2_6mm_exclusive.nii.gz.

--- osl_dynamics/files/parcellation/__init__.py
+++ osl_dynamics/files/parcellation/__init__.py
@@ -5,7 +5,6 @@
 - fMRI_parcellation_ds8mm.nii.gz
 - fmri_d100_parcellation_with_PCC_reduced_2mm_ss5mm_ds8mm.nii.gz (the 'Giles parcellation')
 - fmri_d100_parcellation_with_PCC_tighterMay15_v2_8mm.nii.gz
-- fmri_d100_parcellation_with_PCC_tighterMay15_v2_6mm_exclusive.nii.gz
 - fmri_d100_parcellation_with_3PCC_ips_reduced_2mm_ss5mm_ds8mm.nii.gz
 - fmri_d100_parcellation_with_3PCC_ips_reduced_2mm_ss5mm_ds8mm_adj.nii.gz (used in Viduarre, Nat Comms. 2018)
 - giles_39_binary.nii.gz
