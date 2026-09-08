32ebe78d21bb4de38cd8c694388bb6688a9bb40e
Chetan Gohil <chetan.gohil@psych.ox.ac.uk>
2026-09-08 22:51:12 +0100

    Black formatting.

--- osl_dynamics/models/dynemo.py
+++ osl_dynamics/models/dynemo.py
@@ -606,8 +606,9 @@ class Model(VariationalInferenceModelBase):
         alpha = []
         means = []
         covariances = []
-        with self.set_trainable(fixed_layers, False), set_logging_level(
-            _logger, logging.WARNING
+        with (
+            self.set_trainable(fixed_layers, False),
+            set_logging_level(_logger, logging.WARNING),
         ):
             for i in trange(training_data.n_sessions, desc="Fine tuning"):
                 # Train on this session
